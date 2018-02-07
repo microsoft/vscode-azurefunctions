2275da328bf65c006b80a1c73858d924e8874fcd
Eric Jizba <erijiz@microsoft.com>
2018-02-07 14:02:31 -0800

    Improve error handling for child processes

--- src/utils/cpUtils.ts
+++ src/utils/cpUtils.ts
@@ -12,12 +12,17 @@ export namespace cpUtils {
     export async function executeCommand(outputChannel: vscode.OutputChannel | undefined, workingDirectory: string | undefined, command: string, ...args: string[]): Promise<string> {
         let result: string = '';
         workingDirectory = workingDirectory || os.tmpdir();
+        const formattedArgs: string = args.join(' ');
         await new Promise((resolve: () => void, reject: (e: Error) => void): void => {
             const options: cp.SpawnOptions = {
                 cwd: workingDirectory,
                 shell: true
             };
             const childProc: cp.ChildProcess = cp.spawn(command, args, options);
+
+            if (outputChannel) {
+                outputChannel.appendLine(localize('runningCommand', 'Running command: "{0} {1}"...', command, formattedArgs));
+            }
 
             childProc.stdout.on('data', (data: string | Buffer) => {
                 data = data.toString();
@@ -34,8 +39,17 @@ export namespace cpUtils {
             childProc.on('error', reject);
             childProc.on('close', (code: number) => {
                 if (code !== 0) {
-                    reject(new Error(localize('azFunc.commandError', 'Command "{0} {1}" failed with exit code "{2}".', command, args.toString(), code)));
+                    if (outputChannel) {
+                        outputChannel.show();
+                        reject(new Error(localize('azFunc.commandErrorWithOutput', 'Failed to run "{0}" command. Check output window for more details.', command)));
+                    } else {
+                        // Include all of the output in the error since we couldn't display it directly in the outputChannel
+                        reject(new Error(localize('azFunc.commandError', 'Command "{0} {1}" failed with exit code "{2}":{3}{4}', command, formattedArgs, code, os.EOL, result)));
+                    }
                 } else {
+                    if (outputChannel) {
+                        outputChannel.appendLine(localize('finishedRunningCommand', 'Finished running command: "{0} {1}".', command, formattedArgs));
+                    }
                     resolve();
                 }
             });
