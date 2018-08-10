27575f5658156cd1e5bb569fe5cc17c9d0952df0
Eric Jizba <erijiz@microsoft.com>
2018-08-10 10:50:41 -0700

    Process postActions for .NET Templates

--- tools/JsonCli/src/Program.cs
+++ tools/JsonCli/src/Program.cs
@@ -36,121 +36,123 @@ namespace Microsoft.TemplateEngine.JsonCli
 
         private static int Main(string[] args)
         {
-            TextWriter oldOut = Console.Out;
-            using (Stream fakeOut = new MemoryStream())
-            using (TextWriter fakeOutWriter = new StreamWriter(fakeOut))
-            {
-                Console.SetOut(fakeOutWriter);
+            string profileDir = GetHomeDirectory();
+            string hivePath = Path.Combine(profileDir, ".templateengine", HostName, HostVersion);
 
-                string profileDir = GetHomeDirectory();
-                string hivePath = Path.Combine(profileDir, ".templateengine", HostName, HostVersion);
+            ITemplateEngineHost host = CreateHost(HostName, HostVersion);
+            EnvironmentSettings = new EngineEnvironmentSettings(host, x => new SettingsLoader(x), hivePath);
+            EnvironmentSettings.SettingsLoader.Components.OfType<IIdentifiedComponent>().ToList();
+            _paths = new Paths(EnvironmentSettings);
 
-                ITemplateEngineHost host = CreateHost(HostName, HostVersion);
-                EnvironmentSettings = new EngineEnvironmentSettings(host, x => new SettingsLoader(x), hivePath);
-                EnvironmentSettings.SettingsLoader.Components.OfType<IIdentifiedComponent>().ToList();
-                _paths = new Paths(EnvironmentSettings);
+            //NOTE: With the base directory virtualized, packages cannot be installed from NuGet,
+            //  only local packages and folders
+            EnvironmentSettings.Host.VirtualizeDirectory(_paths.User.Content);
+            EnvironmentSettings.Host.VirtualizeDirectory(_paths.User.Packages);
+            _settingsLoader = (SettingsLoader)EnvironmentSettings.SettingsLoader;
+            _hostDataLoader = new HostSpecificDataLoader(EnvironmentSettings.SettingsLoader);
 
-                //NOTE: With the base directory virtualized, packages cannot be installed from NuGet,
-                //  only local packages and folders
-                EnvironmentSettings.Host.VirtualizeDirectory(_paths.User.Content);
-                EnvironmentSettings.Host.VirtualizeDirectory(_paths.User.Packages);
-                _settingsLoader = (SettingsLoader)EnvironmentSettings.SettingsLoader;
-                _hostDataLoader = new HostSpecificDataLoader(EnvironmentSettings.SettingsLoader);
-
-                CommandLineParser parser = new CommandLineParser(args)
-                    .AddOptionWithConstrainedValue("--operation", new[]
-                    {
+            CommandLineParser parser = new CommandLineParser(args)
+                .AddOptionWithConstrainedValue("--operation", new[]
+                {
                         "list",
                         "create"
-                    })
-                    .AddOptionWithValue("--require")
-                    .AddOptionWithValue("--source");
+                })
+                .AddOptionWithValue("--require")
+                .AddOptionWithValue("--source");
 
-                if (!parser.TryGetValues("--require", out IReadOnlyList<string> requireDirectives)
-                    || !parser.TryGetSingleValue("--operation", out string operation))
-                {
-                    return -1;
-                }
+            if (!parser.TryGetValues("--require", out IReadOnlyList<string> requireDirectives)
+                || !parser.TryGetSingleValue("--operation", out string operation))
+            {
+                Console.SetOut(Console.Error);
+                Console.WriteLine("ERROR: Expected \"--require\" and/or \"--operation\" parameter.");
+                return -1;
+            }
 
-                IInstaller installer = new Installer(EnvironmentSettings);
+            IInstaller installer = new Installer(EnvironmentSettings);
 
-                if (!parser.TryGetValues("--source", out IReadOnlyList<string> sources))
-                {
-                    sources = null;
-                }
+            if (!parser.TryGetValues("--source", out IReadOnlyList<string> sources))
+            {
+                sources = null;
+            }
 
-                installer.InstallPackages(requireDirectives, sources?.ToList());
+            installer.InstallPackages(requireDirectives, sources?.ToList());
 
-                //All required templates/components/lang packs have now been configured
-                //Desired operation information starts at args[commandArgsStart]
+            //All required templates/components/lang packs have now been configured
+            //Desired operation information starts at args[commandArgsStart]
 
-                _templateCreator = new TemplateCreator(EnvironmentSettings);
-                IReadOnlyList<TemplateInfo> rawTemplates = _settingsLoader.UserTemplateCache.TemplateInfo;
+            _templateCreator = new TemplateCreator(EnvironmentSettings);
+            IReadOnlyList<TemplateInfo> rawTemplates = _settingsLoader.UserTemplateCache.TemplateInfo;
 
-                var templates = new JArray();
-                foreach (var rawTemplate in rawTemplates)
-                {
-                    var template = JObject.FromObject(rawTemplate);
-                    template.Add("Parameters", JArray.FromObject(rawTemplate.Parameters));
-                    templates.Add(template);
-                }
+            var templates = new JArray();
+            foreach (var rawTemplate in rawTemplates)
+            {
+                var template = JObject.FromObject(rawTemplate);
+                template.Add("Parameters", JArray.FromObject(rawTemplate.Parameters));
+                templates.Add(template);
+            }
 
 
-                if (string.Equals(operation, "list"))
-                {
-                    Console.SetOut(oldOut);
-                    Console.WriteLine(templates);
-                    return 0;
-                }
-
-                parser = parser.AddOptionWithValue("--identity");
-
-                if (!parser.TryGetSingleValue("--identity", out string identity))
-                {
-                    return -1;
-                }
-
-                TemplateInfo info = rawTemplates.FirstOrDefault(x => string.Equals(x.Identity, identity, StringComparison.Ordinal));
-
-                if (info == null)
-                {
-                    return -1;
-                }
-
-                if (string.Equals(operation, "create"))
-                {
-                    Dictionary<string, string> templateArgs = new Dictionary<string, string>(StringComparer.Ordinal);
-
-                    foreach (ITemplateParameter x in info.Parameters)
-                    {
-                        if (x.DataType != null && x.DataType.StartsWith("bool", StringComparison.OrdinalIgnoreCase))
-                        {
-                            parser = parser.AddOptionWithBooleanValue($"--arg:{x.Name}", string.IsNullOrEmpty(x.DefaultValue) || string.Equals(x.DefaultValue, "true"));
-                        }
-                        else if (x.DataType != null && x.DataType.Equals("choice", StringComparison.OrdinalIgnoreCase))
-                        {
-                            parser = parser.AddOptionWithConstrainedValue($"--arg:{x.Name}", x.Choices.Keys.ToList());
-                        }
-                        else
-                        {
-                            parser = parser.AddOptionWithValue($"--arg:{x.Name}");
-                        }
-
-                        if (parser.TryGetSingleValue($"--arg:{x.Name}", out string val))
-                        {
-                            templateArgs[x.Name] = val;
-                        }
-                    }
-
-                    TemplateCreationResult result = _templateCreator.InstantiateAsync(info, null, null, null, templateArgs, true, false, null).Result;
-
-                    Console.SetOut(oldOut);
-                    Console.WriteLine(JObject.FromObject(result));
-                    return (int)result.Status;
-                }
-
+            if (string.Equals(operation, "list"))
+            {
+                Console.WriteLine(templates);
                 return 0;
             }
+
+            parser = parser.AddOptionWithValue("--identity");
+
+            if (!parser.TryGetSingleValue("--identity", out string identity))
+            {
+                Console.SetOut(Console.Error);
+                Console.WriteLine("ERROR: Expected \"--identity\" parameter.");
+                return -1;
+            }
+
+            TemplateInfo info = rawTemplates.FirstOrDefault(x => string.Equals(x.Identity, identity, StringComparison.Ordinal));
+
+            if (info == null)
+            {
+                Console.SetOut(Console.Error);
+                Console.WriteLine("ERROR: Failed to find template with identity \"{0}\".", identity);
+                return -1;
+            }
+
+            if (string.Equals(operation, "create"))
+            {
+                Dictionary<string, string> templateArgs = new Dictionary<string, string>(StringComparer.Ordinal);
+
+                foreach (ITemplateParameter x in info.Parameters)
+                {
+                    if (x.DataType != null && x.DataType.StartsWith("bool", StringComparison.OrdinalIgnoreCase))
+                    {
+                        parser = parser.AddOptionWithBooleanValue($"--arg:{x.Name}", string.IsNullOrEmpty(x.DefaultValue) || string.Equals(x.DefaultValue, "true"));
+                    }
+                    else if (x.DataType != null && x.DataType.Equals("choice", StringComparison.OrdinalIgnoreCase))
+                    {
+                        parser = parser.AddOptionWithConstrainedValue($"--arg:{x.Name}", x.Choices.Keys.ToList());
+                    }
+                    else
+                    {
+                        parser = parser.AddOptionWithValue($"--arg:{x.Name}");
+                    }
+
+                    if (parser.TryGetSingleValue($"--arg:{x.Name}", out string val))
+                    {
+                        templateArgs[x.Name] = val;
+                    }
+                }
+
+                TemplateCreationResult result = _templateCreator.InstantiateAsync(info, null, null, null, templateArgs, true, false, null).Result;
+                if (result.Status == CreationResultStatus.Success)
+                {
+                    PostActionDispatcher postActionDispatcher = new PostActionDispatcher(EnvironmentSettings, result, AllowPostActionsSetting.Yes);
+                    postActionDispatcher.Process(null);
+                }
+
+                Console.WriteLine(JObject.FromObject(result));
+                return (int)result.Status;
+            }
+
+            return 0;
         }
 
         private static string GetHomeDirectory()
