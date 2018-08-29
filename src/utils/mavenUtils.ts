7fff757eada0ba75e0ab7584125002c0f257f3a5
hanxiao <hanli@microsoft.com>
2018-08-29 19:22:08 +0800

    Fix code style with tslint

--- src/utils/mavenUtils.ts
+++ src/utils/mavenUtils.ts
@@ -7,13 +7,13 @@ import * as fse from 'fs-extra';
 import * as vscode from 'vscode';
 import { TelemetryProperties } from "vscode-azureextensionui";
 import * as xml2js from 'xml2js';
+import { Platform } from '../constants';
 import { localize } from '../localize';
 import { cpUtils } from './cpUtils';
-import { Platform } from '../constants';
 
 export namespace mavenUtils {
     const mvnCommand: string = 'mvn';
-    const quotationMark: string = process.platform == Platform.Windows ? "\"" : "'";
+    const quotationMark: string = process.platform === Platform.Windows ? '"' : '\'';
     export async function validateMavenInstalled(workingDirectory: string | undefined): Promise<void> {
         try {
             await cpUtils.executeCommand(undefined, workingDirectory, mvnCommand, '--version');
@@ -41,7 +41,7 @@ export namespace mavenUtils {
     }
 
     export async function executeMvnCommand(telemetryProperties: TelemetryProperties | undefined, outputChannel: vscode.OutputChannel | undefined, workingDirectory: string | undefined, ...args: string[]): Promise<string> {
-        const result: cpUtils.ICommandResult = await cpUtils.tryExecuteCommand(outputChannel, workingDirectory, mvnCommand, ...args.map((arg) => replaceParameterQuotationMark(arg)));
+        const result: cpUtils.ICommandResult = await cpUtils.tryExecuteCommand(outputChannel, workingDirectory, mvnCommand, ...args.map(replaceParameterQuotationMark));
         if (result.code !== 0) {
             const mvnErrorRegexp: RegExp = new RegExp(/^\[ERROR\](.*)$/, 'gm');
             const linesWithErrors: RegExpMatchArray | null = result.cmdOutputIncludingStderr.match(mvnErrorRegexp);
@@ -68,7 +68,7 @@ export namespace mavenUtils {
     }
 
     function replaceParameterQuotationMark(input: string): string {
-        if (input.startsWith("'") || input.startsWith("\"")) {
+        if (input.startsWith('\'') || input.startsWith('"')) {
             input = quotationMark + input.slice(1, input.length - 1) + quotationMark;
         }
         return input;
