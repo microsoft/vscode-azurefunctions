f0d200e60c8ee7dba049d9df88b3dcde029c1086
Nathan <naturins@microsoft.com>
2018-09-05 18:16:13 -0700

    Add resourceGroup to createFunctionApp api (#551)

--- src/tree/FunctionAppProvider.ts
+++ src/tree/FunctionAppProvider.ts
@@ -71,15 +71,15 @@ export class FunctionAppProvider implements IChildProvider {
         return treeItems;
     }
 
-    public async createChild(parent: IAzureNode, showCreatingNode: (label: string) => void, actionContext: IActionContext): Promise<IAzureTreeItem> {
+    public async createChild(parent: IAzureNode, showCreatingNode: (label: string) => void, userOptions?: { actionContext: IActionContext, resourceGroup?: string }): Promise<IAzureTreeItem> {
         // Ideally actionContext should always be defined, but there's a bug with the NodePicker. Create a 'fake' actionContext until that bug is fixed
         // https://github.com/Microsoft/vscode-azuretools/issues/120
         // tslint:disable-next-line:strict-boolean-expressions
-        actionContext = actionContext || <IActionContext>{ properties: {}, measurements: {} };
-
+        const actionContext: IActionContext = userOptions ? userOptions.actionContext : <IActionContext>{ properties: {}, measurements: {} };
+        const resourceGroup: string | undefined = userOptions ? userOptions.resourceGroup : undefined;
         const runtime: ProjectRuntime = await getDefaultRuntime(actionContext);
         const appSettings: { [key: string]: string } = await getCliFeedAppSettings(runtime);
-        const site: Site = await createFunctionApp(actionContext, parent, showCreatingNode, appSettings);
+        const site: Site = await createFunctionApp(actionContext, parent, showCreatingNode, appSettings, resourceGroup);
         return new FunctionAppTreeItem(new SiteClient(site, parent), this._outputChannel);
     }
 }
