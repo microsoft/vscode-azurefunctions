9ea428fffa3f3aa36709b4caf831b38418f827d2
Eric Jizba <erijiz@microsoft.com>
2018-09-21 10:45:43 -0700

    Better error message when failing to list functions

--- src/tree/FunctionsTreeItem.ts
+++ src/tree/FunctionsTreeItem.ts
@@ -4,6 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { FunctionEnvelope, FunctionEnvelopeCollection } from 'azure-arm-website/lib/models';
+import { isArray } from 'util';
 import { SiteClient } from 'vscode-azureappservice';
 import { createTreeItemsWithErrorHandling, IAzureNode, IAzureParentTreeItem, IAzureTreeItem } from 'vscode-azureextensionui';
 import { localize } from '../localize';
@@ -41,6 +42,11 @@ export class FunctionsTreeItem implements IAzureParentTreeItem {
         }
 
         const funcs: FunctionEnvelopeCollection = this._nextLink ? await this._client.listFunctionsNext(this._nextLink) : await this._client.listFunctions();
+
+        if (!isArray(funcs)) {
+            throw new Error(localize('failedToList', 'Failed to list functions.'));
+        }
+
         this._nextLink = funcs.nextLink;
 
         return await createTreeItemsWithErrorHandling(
