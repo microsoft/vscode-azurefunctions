f91ebac4bbbe6f0a339addc0ec02073942a163ff
Eric Jizba <erijiz@microsoft.com>
2018-10-01 15:57:50 -0700

    Fix getNpmDistTag

--- src/funcCoreTools/getNpmDistTag.ts
+++ src/funcCoreTools/getNpmDistTag.ts
@@ -5,21 +5,36 @@
 
 // tslint:disable-next-line:no-require-imports
 import request = require('request-promise');
+import * as semver from 'semver';
 import { ProjectRuntime } from '../constants';
 import { localize } from '../localize';
 
-const npmRegistryUri: string = 'https://aka.ms/W2mvv3';
+const npmRegistryUri: string = 'https://aka.ms/AA2qmnu';
 
 export interface INpmDistTag { tag: string; value: string; }
 
+interface IPackageMetadata {
+    versions: { [version: string]: {} };
+}
+
 export async function getNpmDistTag(runtime: ProjectRuntime): Promise<INpmDistTag> {
-    const tags: { [key: string]: string } = <{ [key: string]: string }>JSON.parse(await <Thenable<string>>request(npmRegistryUri));
-    for (const key of Object.keys(tags)) {
-        if ((runtime === ProjectRuntime.v1 && tags[key].startsWith('1')) ||
-            (runtime === ProjectRuntime.v2 && tags[key].startsWith('2'))) {
-            return { tag: key, value: tags[key] };
-        }
+    const packageMetadata: IPackageMetadata = <IPackageMetadata>JSON.parse(await <Thenable<string>>request(npmRegistryUri));
+    let majorVersion: string;
+    switch (runtime) {
+        case ProjectRuntime.v1:
+            majorVersion = '1';
+            break;
+        case ProjectRuntime.v2:
+            majorVersion = '2';
+            break;
+        default:
+            throw new RangeError(localize('invalidRuntime', 'Invalid runtime "{0}".', runtime));
     }
 
-    throw new Error(localize('noDistTag', 'Failed to retrieve NPM tag for runtime "{0}".', runtime));
+    const filteredVersions: string[] = Object.keys(packageMetadata.versions).filter((v: string) => v.startsWith(majorVersion));
+    if (filteredVersions.length < 1) {
+        throw new Error(localize('noDistTag', 'Failed to retrieve NPM tag for runtime "{0}".', runtime));
+    }
+    const maxVersion: string = filteredVersions.reduce((v1: string, v2: string) => semver.gt(v1, v2) ? v1 : v2);
+    return { tag: majorVersion, value: maxVersion };
 }
