82bc21c3f072cf621728b5d0124c0814cf145985
nturinski <naturins@microsoft.com>
2018-10-18 17:13:51 -0700

    Add host attribute

--- src/commands/createNewProject/PythonProjectCreator.ts
+++ src/commands/createNewProject/PythonProjectCreator.ts
@@ -113,7 +113,7 @@ export class PythonProjectCreator extends ScriptProjectCreatorBase {
                     },
                     options: {
                         env: {
-                            languageWorkers__python__arguments: '-m ptvsd --server --port 9091 --file'
+                            languageWorkers__python__arguments: '-m ptvsd --server --port 9091 --host localhost --file'
                         }
                     },
                     problemMatcher: funcWatchProblemMatcher
