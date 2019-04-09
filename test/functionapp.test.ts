0ddb34f57d03c0e1b6a7ac3f12fb17b23d0ac292
unknown <AzureTest@vwuzhai002.redmond.corp.microsoft.com>
2019-04-09 00:15:24 -0700

    Modify the FTE comments

--- test/functionapp.test.ts
+++ test/functionapp.test.ts
@@ -8,7 +8,7 @@ import { ResourceManagementClient } from 'azure-arm-resource';
 import { WebSiteManagementClient, WebSiteManagementModels } from 'azure-arm-website';
 import { IHookCallbackContext, ISuiteCallbackContext } from 'mocha';
 import * as vscode from 'vscode';
-import { AzureTreeDataProvider, DialogResponses, ext, extensionPrefix, FunctionAppProvider, getRandomHexString, TestAzureAccount, TestUserInput } from '../extension.bundle';
+import { AzureTreeDataProvider, DialogResponses, ext, extensionPrefix, FunctionAppProvider, getGlobalFuncExtensionSetting, getRandomHexString, ProjectLanguage, projectLanguageSetting, TestAzureAccount, TestUserInput, updateGlobalSetting } from '../extension.bundle';
 import { longRunningTestsEnabled } from './global.test';
 
 // tslint:disable-next-line:max-func-body-length
@@ -16,7 +16,8 @@ suite('Function App actions', async function (this: ISuiteCallbackContext): Prom
     this.timeout(1200 * 1000);
     const resourceGroupsToDelete: string[] = [];
     let oldAdvancedCreationSetting: boolean | undefined;
-    const resourceName: string = getRandomHexString().toLowerCase();
+    let oldProjectLanguage: ProjectLanguage | undefined;
+    const resourceName1: string = getRandomHexString().toLowerCase();
     const testAccount: TestAzureAccount = new TestAzureAccount();
 
     suiteSetup(async function (this: IHookCallbackContext): Promise<void> {
@@ -24,6 +25,7 @@ suite('Function App actions', async function (this: ISuiteCallbackContext): Prom
             this.skip();
         }
         this.timeout(120 * 1000);
+        oldProjectLanguage = getGlobalFuncExtensionSetting(projectLanguageSetting);
         oldAdvancedCreationSetting = <boolean>vscode.workspace.getConfiguration(extensionPrefix).get('advancedCreation');
         await testAccount.signIn();
         ext.tree = new AzureTreeDataProvider(FunctionAppProvider, 'azureFunctions.startTesting', undefined, testAccount);
@@ -34,6 +36,7 @@ suite('Function App actions', async function (this: ISuiteCallbackContext): Prom
             this.skip();
         }
         this.timeout(1200 * 1000);
+        await updateGlobalSetting(projectLanguageSetting, oldProjectLanguage);
         await vscode.workspace.getConfiguration(extensionPrefix).update('advancedCreation', oldAdvancedCreationSetting, vscode.ConfigurationTarget.Global);
         const client: ResourceManagementClient = getResourceManagementClient(testAccount);
         for (const resourceGroup of resourceGroupsToDelete) {
@@ -49,10 +52,22 @@ suite('Function App actions', async function (this: ISuiteCallbackContext): Prom
         ext.tree.dispose();
     });
 
-    test('createFunctionApp (basic)', async () => {
-        const resourceName1: string = getRandomHexString().toLowerCase();
+    test('createFunctionApp (Basic)', async () => {
+        const resourceName2: string = getRandomHexString().toLowerCase();
+        resourceGroupsToDelete.push(resourceName2);
+        await vscode.workspace.getConfiguration(extensionPrefix).update('advancedCreation', false, vscode.ConfigurationTarget.Global);
+        await updateGlobalSetting(projectLanguageSetting, ProjectLanguage.JavaScript);
+        ext.ui = new TestUserInput([resourceName2]);
+        await vscode.commands.executeCommand('azureFunctions.createFunctionApp');
+        const client: WebSiteManagementClient = getWebsiteManagementClient(testAccount);
+        const createdApp: WebSiteManagementModels.Site = await client.webApps.get(resourceName2, resourceName2);
+        assert.ok(createdApp);
+    });
+
+    test('createFunctionApp (Advanced)', async () => {
         resourceGroupsToDelete.push(resourceName1);
-        const testInputs: string[] = [resourceName1, '$(plus) Create new resource group', resourceName1, '$(plus) Create new storage account', resourceName1, 'East US'];
+        await vscode.workspace.getConfiguration(extensionPrefix).update('advancedCreation', true, vscode.ConfigurationTarget.Global);
+        const testInputs: string[] = [resourceName1, 'Linux', '.NET', '$(plus) Create new resource group', resourceName1, '$(plus) Create new storage account', resourceName1, 'West US'];
         ext.ui = new TestUserInput(testInputs);
         await vscode.commands.executeCommand('azureFunctions.createFunctionApp');
         const client: WebSiteManagementClient = getWebsiteManagementClient(testAccount);
@@ -60,54 +75,43 @@ suite('Function App actions', async function (this: ISuiteCallbackContext): Prom
         assert.ok(createdApp);
     });
 
-    test('createFunctionApp (Advanced)', async () => {
-        resourceGroupsToDelete.push(resourceName);
-        await vscode.workspace.getConfiguration(extensionPrefix).update('advancedCreation', true, vscode.ConfigurationTarget.Global);
-        const testInputs: string[] = [resourceName, 'Linux', '.NET', '$(plus) Create new resource group', resourceName, '$(plus) Create new storage account', resourceName, 'West US'];
-        ext.ui = new TestUserInput(testInputs);
-        await vscode.commands.executeCommand('azureFunctions.createFunctionApp');
-        const client: WebSiteManagementClient = getWebsiteManagementClient(testAccount);
-        const createdApp: WebSiteManagementModels.Site = await client.webApps.get(resourceName, resourceName);
-        assert.ok(createdApp);
-    });
-
     test('stopFunctionApp', async () => {
-        ext.ui = new TestUserInput([resourceName]);
+        ext.ui = new TestUserInput([resourceName1]);
         await vscode.commands.executeCommand('azureFunctions.stopFunctionApp');
         const client: WebSiteManagementClient = getWebsiteManagementClient(testAccount);
-        const createdApp: WebSiteManagementModels.Site = await client.webApps.get(resourceName, resourceName);
+        const createdApp: WebSiteManagementModels.Site = await client.webApps.get(resourceName1, resourceName1);
         assert.equal(createdApp.state, 'Stopped', `Function App state should be 'Stopped' rather than ${createdApp.state}.`);
     });
 
     test('startFunctionApp', async () => {
-        ext.ui = new TestUserInput([resourceName]);
+        ext.ui = new TestUserInput([resourceName1]);
         await vscode.commands.executeCommand('azureFunctions.startFunctionApp');
         const client: WebSiteManagementClient = getWebsiteManagementClient(testAccount);
-        const createdApp: WebSiteManagementModels.Site = await client.webApps.get(resourceName, resourceName);
+        const createdApp: WebSiteManagementModels.Site = await client.webApps.get(resourceName1, resourceName1);
         assert.equal(createdApp.state, 'Running', `Function App state should be 'Running' rather than ${createdApp.state}.`);
     });
 
     test('restartFunctionApp', async () => {
         let client: WebSiteManagementClient;
         let createdApp: WebSiteManagementModels.Site;
-        ext.ui = new TestUserInput([resourceName]);
+        ext.ui = new TestUserInput([resourceName1]);
         await vscode.commands.executeCommand('azureFunctions.stopFunctionApp');
         client = getWebsiteManagementClient(testAccount);
-        createdApp = await client.webApps.get(resourceName, resourceName);
+        createdApp = await client.webApps.get(resourceName1, resourceName1);
         assert.equal(createdApp.state, 'Stopped', `Function App state should be 'Stopped' rather than ${createdApp.state}.`);
 
-        ext.ui = new TestUserInput([resourceName, resourceName]);
+        ext.ui = new TestUserInput([resourceName1, resourceName1]);
         await vscode.commands.executeCommand('azureFunctions.restartFunctionApp');
         client = getWebsiteManagementClient(testAccount);
-        createdApp = await client.webApps.get(resourceName, resourceName);
+        createdApp = await client.webApps.get(resourceName1, resourceName1);
         assert.equal(createdApp.state, 'Running', `Function App state should be 'Running' rather than ${createdApp.state}.`);
     });
 
     test('deleteFunctionApp', async () => {
-        ext.ui = new TestUserInput([resourceName, DialogResponses.deleteResponse.title, DialogResponses.yes.title]);
+        ext.ui = new TestUserInput([resourceName1, DialogResponses.deleteResponse.title, DialogResponses.yes.title]);
         await vscode.commands.executeCommand('azureFunctions.deleteFunctionApp');
         const client: WebSiteManagementClient = getWebsiteManagementClient(testAccount);
-        const deletedApp: WebSiteManagementModels.Site | undefined = await client.webApps.get(resourceName, resourceName);
+        const deletedApp: WebSiteManagementModels.Site | undefined = await client.webApps.get(resourceName1, resourceName1);
         assert.ifError(deletedApp);
     });
 });
