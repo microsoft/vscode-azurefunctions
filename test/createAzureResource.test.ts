f1908e86eb8f2cac454a72bc5e92380b67ad6616
unknown <AzureTest@vwuzhai002.redmond.corp.microsoft.com>
2019-04-16 03:34:16 -0700

    Add tests for deploying CSharp and JavaScript projects

--- test/createAzureResource.test.ts
+++ test/createAzureResource.test.ts
@@ -6,29 +6,31 @@
 import * as assert from 'assert';
 import { ResourceManagementClient } from 'azure-arm-resource';
 import { WebSiteManagementClient, WebSiteManagementModels } from 'azure-arm-website';
+import { execSync } from 'child_process';
+import * as clipboard from 'clipboardy';
+import * as fse from 'fs-extra';
 import { IHookCallbackContext, ISuiteCallbackContext } from 'mocha';
+import * as path from 'path';
 import * as vscode from 'vscode';
-import { AzureTreeDataProvider, DialogResponses, ext, FunctionAppProvider, getGlobalSetting, getRandomHexString, ProjectLanguage, projectLanguageSetting, TestAzureAccount, TestUserInput, updateGlobalSetting } from '../extension.bundle';
+import { AzureTreeDataProvider, DialogResponses, ext, FunctionAppProvider, getRandomHexString, ProjectLanguage, ProjectRuntime, TestAzureAccount, TestUserInput } from '../extension.bundle';
 import { longRunningTestsEnabled } from './global.test';
 import { runWithFuncSetting } from './runWithSetting';
+import { getCSharpValidateOptions, getJavaScriptValidateOptions, validateProject } from './validateProject';
 
 // tslint:disable-next-line:max-func-body-length
 suite('Create Azure Resources', async function (this: ISuiteCallbackContext): Promise<void> {
     this.timeout(1200 * 1000);
     const resourceGroupsToDelete: string[] = [];
     const testAccount: TestAzureAccount = new TestAzureAccount();
-    let oldProjectLanguage: ProjectLanguage | undefined;
     let webSiteClient: WebSiteManagementClient;
     const resourceName1: string = getRandomHexString().toLowerCase();
+    const resourceName3: string = getRandomHexString().toLowerCase();
+    const projectPath: string = path.join('C:', 'testOutput');
 
     suiteSetup(async function (this: IHookCallbackContext): Promise<void> {
         if (!longRunningTestsEnabled) {
             this.skip();
         }
-
-        // set project language so that test isn't prompted for runtime
-        oldProjectLanguage = getGlobalSetting(projectLanguageSetting);
-        await updateGlobalSetting(projectLanguageSetting, ProjectLanguage.JavaScript);
 
         this.timeout(120 * 1000);
         await testAccount.signIn();
@@ -41,8 +43,6 @@ suite('Create Azure Resources', async function (this: ISuiteCallbackContext): Pr
             this.skip();
         }
         this.timeout(1200 * 1000);
-
-        await updateGlobalSetting(projectLanguageSetting, oldProjectLanguage);
 
         const client: ResourceManagementClient = getResourceManagementClient(testAccount);
         for (const resourceGroup of resourceGroupsToDelete) {
@@ -58,14 +58,38 @@ suite('Create Azure Resources', async function (this: ISuiteCallbackContext): Pr
         ext.tree.dispose();
     });
 
-    test('createFunctionApp (Basic)', async () => {
+    test('Create windows function app (Basic) and deploy JavaScript project', async () => {
+        const functionName: string = 'HttpTrigger';
         resourceGroupsToDelete.push(resourceName1);
-        await runWithFuncSetting('advancedCreation', undefined, async () => {
-            ext.ui = new TestUserInput([resourceName1]);
-            await vscode.commands.executeCommand('azureFunctions.createFunctionApp');
-            const createdApp: WebSiteManagementModels.Site = await webSiteClient.webApps.get(resourceName1, resourceName1);
-            assert.ok(createdApp);
+        await clearnewprojectfile(projectPath);
+        await runWithFuncSetting('projectLanguage', ProjectLanguage.JavaScript, async () => {
+            await runWithFuncSetting('advancedCreation', undefined, async () => {
+                ext.ui = new TestUserInput([resourceName1]);
+                await vscode.commands.executeCommand('azureFunctions.createFunctionApp');
+                const createdApp: WebSiteManagementModels.Site = await webSiteClient.webApps.get(resourceName1, resourceName1);
+                assert.ok(createdApp);
+                await runWithFuncSetting('projectRuntime', ProjectRuntime.v2, async () => {
+                    const templateId: string = 'HttpTrigger-JavaScript';
+                    const authLevel: string = 'function';
+                    await vscode.commands.executeCommand('azureFunctions.createNewProject', projectPath, ProjectLanguage.JavaScript, ProjectRuntime.v2, false, templateId, functionName, { aUtHLevel: authLevel });
+                    await validateProject(projectPath, getJavaScriptValidateOptions());
+                });
+            });
         });
+
+        ext.ui = new TestUserInput([resourceName1, 'Deploy']);
+        await vscode.commands.executeCommand('azureFunctions.deploy', projectPath);
+        const getFunction: WebSiteManagementModels.FunctionEnvelopeCollection = await webSiteClient.webApps.listFunctions(resourceName1, resourceName1);
+        assert.equal(getFunction[0].name, `${resourceName1}/${functionName}`);
+
+        clipboard.writeSync(typeof (undefined));
+        ext.ui = new TestUserInput([resourceName1, functionName]);
+        await vscode.commands.executeCommand('azureFunctions.copyFunctionUrl');
+        assert.ok(clipboard.readSync() !== 'undefined', `The value of the copy function url should not be ${clipboard.readSync()}`);
+        assert.ok(clipboard.readSync().includes(resourceName1));
+
+        const result: string = execSync(`curl ${clipboard.readSync()}"&"name=${resourceName1}`).toString();
+        assert.equal(result, `Hello ${resourceName1}`, `The return value should be Hello ${resourceName1}`);
     });
 
     test('createFunctionApp (Advanced)', async () => {
@@ -119,6 +143,41 @@ suite('Create Azure Resources', async function (this: ISuiteCallbackContext): Pr
         assert.ifError(deletedApp);
     });
 
+    test('Deploy CSharp project (windows)', async () => {
+        await clearnewprojectfile(projectPath);
+        const functionName: string = 'HttpTriggerCSharp';
+        resourceGroupsToDelete.push(resourceName3);
+        await runWithFuncSetting('projectLanguage', ProjectLanguage.CSharp, async () => {
+            await runWithFuncSetting('advancedCreation', undefined, async () => {
+                ext.ui = new TestUserInput([resourceName3]);
+                await vscode.commands.executeCommand('azureFunctions.createFunctionApp');
+                const createdApp: WebSiteManagementModels.Site = await webSiteClient.webApps.get(resourceName3, resourceName3);
+                assert.ok(createdApp);
+                await runWithFuncSetting('projectRuntime', ProjectRuntime.v2, async () => {
+                    const templateId: string = 'Azure.Function.CSharp.HttpTrigger.2.x';
+                    const namespace: string = 'Company.Function';
+                    const accessrights: string = 'function';
+                    await vscode.commands.executeCommand('azureFunctions.createNewProject', projectPath, ProjectLanguage.CSharp, ProjectRuntime.v2, false, templateId, functionName, { namespace: namespace, AccessRights: accessrights });
+                    await validateProject(projectPath, getCSharpValidateOptions('testOutput', 'netcoreapp2.1'));
+                });
+            });
+        });
+
+        ext.ui = new TestUserInput([resourceName3, 'Deploy']);
+        await vscode.commands.executeCommand('azureFunctions.deploy', projectPath);
+        const getFunction: WebSiteManagementModels.FunctionEnvelopeCollection = await webSiteClient.webApps.listFunctions(resourceName3, resourceName3);
+        assert.equal(getFunction[0].name, `${resourceName3}/${functionName}`);
+
+        clipboard.writeSync(typeof (undefined));
+        ext.ui = new TestUserInput([resourceName3, functionName]);
+        await vscode.commands.executeCommand('azureFunctions.copyFunctionUrl');
+        assert.notEqual(clipboard.readSync(), 'undefined', `The value of the copy function url should not be "${clipboard.readSync()}"`);
+        assert.ok(clipboard.readSync().includes(resourceName3));
+
+        const result: string = execSync(`curl ${clipboard.readSync()}"&"name=${resourceName3}`).toString();
+        assert.equal(result, `Hello, ${resourceName3}`, `The return value should be Hello, ${resourceName3}`);
+    });
+
     // https://github.com/Microsoft/vscode-azurefunctions/blob/master/docs/api.md#create-function-app
     test('createFunctionApp API', async () => {
         const resourceGroupName: string = getRandomHexString();
@@ -153,4 +212,17 @@ function getWebsiteManagementClient(testAccount: TestAzureAccount): WebSiteManag
 
 function getResourceManagementClient(testAccount: TestAzureAccount): ResourceManagementClient {
     return new ResourceManagementClient(testAccount.getSubscriptionCredentials(), testAccount.getSubscriptionId());
+}
+
+async function clearnewprojectfile(projectpath: string): Promise<void> {
+    let files: string[] = [];
+    if (fse.existsSync(projectpath)) {
+        files = fse.readdirSync(projectpath);
+        console.log(`Deleting the folder "${projectpath}"...`);
+        files.forEach((file: string) => {
+            const curPath: string = path.join(projectpath, file);
+            fse.removeSync(curPath);
+        });
+    }
+    console.log(`The folder "${projectpath}" is cleared`);
 }
