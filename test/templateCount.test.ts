9ce33f7c8b4a5d2073452635e9621abbd44371b1
Eric Jizba <erijiz@microsoft.com>
2019-04-24 11:10:38 -0700

    Improve "Create Function" tests
    1. Actually verify the settings passed in
    1. Don't use "vscode.command.executeCommand" becuase that swallows exceptions
    1. Validate that there is a test for every "Verified" template

--- test/templateCount.test.ts
+++ test/templateCount.test.ts
@@ -5,7 +5,7 @@
 
 import * as assert from 'assert';
 import { IFunctionTemplate, ProjectLanguage, ProjectRuntime, TemplateFilter, TemplateProvider, TemplateSource } from '../extension.bundle';
-import { runForTemplateSource } from './global.test';
+import { runForTemplateSource, testFolderPath } from './global.test';
 
 addSuite(undefined);
 addSuite(TemplateSource.CliFeed);
@@ -13,59 +13,59 @@ addSuite(TemplateSource.StagingCliFeed);
 addSuite(TemplateSource.Backup);
 
 function addSuite(source: TemplateSource | undefined): void {
-    suite(`Verified Template Count - ${source === undefined ? 'defaultOnExtensionActivation' : source}`, async () => {
+    suite(`Template Count - ${source === undefined ? 'defaultOnExtensionActivation' : source}`, async () => {
         test('JavaScript v1', async () => {
             await runForTemplateSource(source, async (templates: TemplateProvider) => {
-                const jsTemplatesv1: IFunctionTemplate[] = await templates.getTemplates(ProjectLanguage.JavaScript, ProjectRuntime.v1, TemplateFilter.Verified);
+                const jsTemplatesv1: IFunctionTemplate[] = await templates.getTemplates(ProjectLanguage.JavaScript, ProjectRuntime.v1, testFolderPath, TemplateFilter.Verified);
                 assert.equal(jsTemplatesv1.length, 8);
             });
         });
 
         test('JavaScript v2', async () => {
             await runForTemplateSource(source, async (templates: TemplateProvider) => {
-                const jsTemplatesv2: IFunctionTemplate[] = await templates.getTemplates(ProjectLanguage.JavaScript, ProjectRuntime.v2, TemplateFilter.Verified);
+                const jsTemplatesv2: IFunctionTemplate[] = await templates.getTemplates(ProjectLanguage.JavaScript, ProjectRuntime.v2, testFolderPath, TemplateFilter.Verified);
                 assert.equal(jsTemplatesv2.length, 12);
             });
         });
 
         test('Java v2', async () => {
             await runForTemplateSource(source, async (templates: TemplateProvider) => {
-                const javaTemplates: IFunctionTemplate[] = await templates.getTemplates(ProjectLanguage.Java, ProjectRuntime.v2, TemplateFilter.Verified);
+                const javaTemplates: IFunctionTemplate[] = await templates.getTemplates(ProjectLanguage.Java, ProjectRuntime.v2, testFolderPath, TemplateFilter.Verified);
                 assert.equal(javaTemplates.length, 4);
             });
         });
 
         test('C# v1', async () => {
             await runForTemplateSource(source, async (templates: TemplateProvider) => {
-                const cSharpTemplates: IFunctionTemplate[] = await templates.getTemplates(ProjectLanguage.CSharp, ProjectRuntime.v1, TemplateFilter.Verified);
+                const cSharpTemplates: IFunctionTemplate[] = await templates.getTemplates(ProjectLanguage.CSharp, ProjectRuntime.v1, testFolderPath, TemplateFilter.Verified);
                 assert.equal(cSharpTemplates.length, 12);
             });
         });
 
         test('C# v2', async () => {
             await runForTemplateSource(source, async (templates: TemplateProvider) => {
-                const cSharpTemplatesv2: IFunctionTemplate[] = await templates.getTemplates(ProjectLanguage.CSharp, ProjectRuntime.v2, TemplateFilter.Verified);
+                const cSharpTemplatesv2: IFunctionTemplate[] = await templates.getTemplates(ProjectLanguage.CSharp, ProjectRuntime.v2, testFolderPath, TemplateFilter.Verified);
                 assert.equal(cSharpTemplatesv2.length, 9);
             });
         });
 
         test('Python v2', async () => {
             await runForTemplateSource(source, async (templates: TemplateProvider) => {
-                const pythonTemplates: IFunctionTemplate[] = await templates.getTemplates(ProjectLanguage.Python, ProjectRuntime.v2, TemplateFilter.Verified);
+                const pythonTemplates: IFunctionTemplate[] = await templates.getTemplates(ProjectLanguage.Python, ProjectRuntime.v2, testFolderPath, TemplateFilter.Verified);
                 assert.equal(pythonTemplates.length, 9);
             });
         });
 
         test('TypeScript v2', async () => {
             await runForTemplateSource(source, async (templates: TemplateProvider) => {
-                const tsTemplates: IFunctionTemplate[] = await templates.getTemplates(ProjectLanguage.TypeScript, ProjectRuntime.v2, TemplateFilter.Verified);
+                const tsTemplates: IFunctionTemplate[] = await templates.getTemplates(ProjectLanguage.TypeScript, ProjectRuntime.v2, testFolderPath, TemplateFilter.Verified);
                 assert.equal(tsTemplates.length, 12);
             });
         });
 
         test('PowerShell v2', async () => {
             await runForTemplateSource(source, async (templates: TemplateProvider) => {
-                const powershellTemplates: IFunctionTemplate[] = await templates.getTemplates(ProjectLanguage.PowerShell, ProjectRuntime.v2, TemplateFilter.Verified);
+                const powershellTemplates: IFunctionTemplate[] = await templates.getTemplates(ProjectLanguage.PowerShell, ProjectRuntime.v2, testFolderPath, TemplateFilter.Verified);
                 assert.equal(powershellTemplates.length, 9);
             });
         });
