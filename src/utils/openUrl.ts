f907832c94fa5008735e92e66c5b8be7aa40b5f7
Eric Jizba <erijiz@microsoft.com>
2019-08-07 14:26:45 -0700

    "Support" log streaming for linux function apps

--- src/utils/openUrl.ts
+++ src/utils/openUrl.ts
@@ -3,8 +3,14 @@
  *  Licensed under the MIT License. See License.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as vscode from 'vscode';
+// tslint:disable-next-line:no-require-imports
+import opn = require("opn");
 
 export async function openUrl(url: string): Promise<void> {
-    await vscode.env.openExternal(vscode.Uri.parse(url));
+    // Using this functionality is blocked by https://github.com/Microsoft/vscode/issues/25852
+    // Specifically, opening the Live Metrics Stream for Linux Function Apps doesn't work in this extension.
+    // await vscode.env.openExternal(vscode.Uri.parse(url));
+
+    // tslint:disable-next-line: no-unsafe-any
+    opn(url);
 }
