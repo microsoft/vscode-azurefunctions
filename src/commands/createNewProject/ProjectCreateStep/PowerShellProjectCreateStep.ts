b363b26d5cce26131818cf3df9cd597e0725f85a
Eric Jizba <erijiz@microsoft.com>
2019-10-16 15:28:51 -0700

    Remove comment in requirements.psd1 🗑 (#1589)

--- src/commands/createNewProject/ProjectCreateStep/PowerShellProjectCreateStep.ts
+++ src/commands/createNewProject/ProjectCreateStep/PowerShellProjectCreateStep.ts
@@ -37,7 +37,6 @@ if ($env:MSI_SECRET -and (Get-Module -ListAvailable Az.Accounts)) {
 `;
 
 const requirementspsd1: string = `# This file enables modules to be automatically managed by the Functions service.
-# Only the Azure Az module is supported in preview.
 # See https://aka.ms/functionsmanageddependency for additional information.
 #
 @{
