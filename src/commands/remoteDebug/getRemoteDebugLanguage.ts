4fea21ab566c503ce37e9633153c57aee9203960
Michael Crane <michael.crane@gmail.com>
2019-10-24 16:22:23 -0700

    Refactor remote debugging to support different languages in the future (#1488)

--- src/commands/remoteDebug/getRemoteDebugLanguage.ts
+++ src/commands/remoteDebug/getRemoteDebugLanguage.ts
@@ -4,15 +4,16 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { WebSiteManagementModels } from 'azure-arm-website';
+import { RemoteDebugLanguage } from 'vscode-azureappservice';
 
-export function checkForRemoteDebugSupport(siteConfig: WebSiteManagementModels.SiteConfig): void {
+export function getRemoteDebugLanguage(siteConfig: WebSiteManagementModels.SiteConfig): RemoteDebugLanguage {
     // Read siteConfig.linuxFxVersion to determine debugging support
     //   If the Function App is running on Windows, it will be empty
     //   If the Function App is running on Linux consumption, it will be empty
     //   If the Function App is running on a Linux App Service plan, it will contain Docker registry information, e.g. "DOCKER|repo.azurecr.io/image:tag"
     //      The blessed Node.js Docker image will be something like "DOCKER|mcr.microsoft.com/azure-functions/node:2.0-node8-appservice"
     if (siteConfig.linuxFxVersion && siteConfig.linuxFxVersion.startsWith('DOCKER|mcr.microsoft.com/azure-functions/node')) {
-        return;
+        return RemoteDebugLanguage.Node;
     }
 
     throw new Error('Azure Remote Debugging is currently only supported for Node.js Function Apps running on Linux App Service plans. Consumption plans are not supported.');
