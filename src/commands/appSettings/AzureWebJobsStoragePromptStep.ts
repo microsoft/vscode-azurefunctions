978cdab9cdbf58fe6f5d73356dcd68d76e12e6ba
Eric Jizba <erijiz@microsoft.com>
2019-11-21 12:06:14 -0800

    Revert "Allow local emulator on mac/linux (#1690)"
    This reverts commit 21c08aecc1f8968ed87b6af47e85206422e2a183.

--- src/commands/appSettings/AzureWebJobsStoragePromptStep.ts
+++ src/commands/appSettings/AzureWebJobsStoragePromptStep.ts
@@ -24,7 +24,11 @@ export class AzureWebJobsStoragePromptStep<T extends IAzureWebJobsStorageWizardC
 
         const message: string = localize('selectAzureWebJobsStorage', 'In order to debug, you must select a storage account for internal use by the Azure Functions runtime.');
 
-        const buttons: MessageItem[] = [selectAccount, useEmulator];
+        const buttons: MessageItem[] = [selectAccount];
+        if (process.platform === 'win32') {
+            // Only show on Windows until this is fixed: https://github.com/Microsoft/vscode-azurefunctions/issues/1245
+            buttons.push(useEmulator);
+        }
         if (!this._suppressSkipForNow) {
             buttons.push(skipForNow);
         }
