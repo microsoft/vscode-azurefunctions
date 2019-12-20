c1f426f8498f34a16eb5e92b71962bc463df2389
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2019-12-20 14:45:53 -0800

    Fix psd1 template

--- src/commands/createNewProject/ProjectCreateStep/PowerShellProjectCreateStep.ts
+++ src/commands/createNewProject/ProjectCreateStep/PowerShellProjectCreateStep.ts
@@ -51,8 +51,7 @@ const requirementspsd1Offine: string = `# This file enables modules to be automa
 # See https://aka.ms/functionsmanageddependency for additional information.
 #
 @{
-    # For latest supported version, go to 'https://www.powershellgallery.com/packages/Az'.
-    # Uncomment the next line and replace the MAJOR_VERSION, e.g., 'Az' = '3.*'
+    # For latest supported version, go to 'https://www.powershellgallery.com/packages/Az'. Uncomment the next line and replace the MAJOR_VERSION, e.g., 'Az' = '3.*'
     # 'Az' = 'MAJOR_VERSION.*'
 }`;
 
