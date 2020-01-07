3119726e979c13a6f0478b00f8b6228503506d5a
Hanzhang Zeng (Roger) <48038149+Hazhzeng@users.noreply.github.com>
2020-01-07 10:54:06 -0800

    Handle lack of internet connection or if the www.powershellgallery.com is down when creating a PowerShell function app (#1764)

--- src/commands/createNewProject/ProjectCreateStep/PowerShellProjectCreateStep.ts
+++ src/commands/createNewProject/ProjectCreateStep/PowerShellProjectCreateStep.ts
@@ -6,13 +6,15 @@
 import * as fse from 'fs-extra';
 import * as path from 'path';
 import { Progress } from 'vscode';
+import * as xml2js from 'xml2js';
+import { localize } from '../../../localize';
 import { confirmOverwriteFile } from "../../../utils/fs";
+import { requestUtils } from '../../../utils/requestUtils';
 import { IProjectWizardContext } from '../IProjectWizardContext';
 import { ScriptProjectCreateStep } from './ScriptProjectCreateStep';
 
 const profileps1FileName: string = 'profile.ps1';
 const requirementspsd1FileName: string = 'requirements.psd1';
-
 const profileps1: string = `# Azure Functions profile.ps1
 #
 # This profile.ps1 will get executed every "cold start" of your Function App.
@@ -36,15 +38,28 @@ if ($env:MSI_SECRET -and (Get-Module -ListAvailable Az.Accounts)) {
 # You can also define functions or aliases that can be referenced in any of your PowerShell functions.
 `;
 
-const requirementspsd1: string = `# This file enables modules to be automatically managed by the Functions service.
+function requirementspsd1(majorVersion: number): string {
+    return `# This file enables modules to be automatically managed by the Functions service.
 # See https://aka.ms/functionsmanageddependency for additional information.
 #
 @{
-    'Az' = '3.*'
+    'Az' = '${majorVersion}.*'
+}`;
+}
+
+const requirementspsd1Offine: string = `# This file enables modules to be automatically managed by the Functions service.
+# See https://aka.ms/functionsmanageddependency for additional information.
+#
+@{
+    # For latest supported version, go to 'https://www.powershellgallery.com/packages/Az'. Uncomment the next line and replace the MAJOR_VERSION, e.g., 'Az' = '3.*'
+    # 'Az' = 'MAJOR_VERSION.*'
 }`;
 
 export class PowerShellProjectCreateStep extends ScriptProjectCreateStep {
     protected supportsManagedDependencies: boolean = true;
+
+    private readonly azModuleName: string = 'Az';
+    private readonly azModuleGalleryUrl: string = `https://aka.ms/PwshPackageInfo?id='${this.azModuleName}'`;
 
     public async executeCore(context: IProjectWizardContext, progress: Progress<{ message?: string | undefined; increment?: number | undefined }>): Promise<void> {
         await super.executeCore(context, progress);
@@ -54,9 +69,79 @@ export class PowerShellProjectCreateStep extends ScriptProjectCreateStep {
             await fse.writeFile(profileps1Path, profileps1);
         }
 
+        const majorVersion: number | undefined = await this.tryGetLatestAzModuleMajorVersion(progress);
+        if (majorVersion !== undefined) {
+            progress.report({
+                message: localize('successfullyConnected', 'Successfully retrieved {0} information from PowerShell Gallery', this.azModuleName)
+            });
+        } else {
+            progress.report({
+                message: localize('failedToConnect', 'Failed to get {0} module version. Edit the requirements.psd1 file when the powershellgallery.com is accessible.', this.azModuleName)
+            });
+        }
+
         const requirementspsd1Path: string = path.join(context.projectPath, requirementspsd1FileName);
         if (await confirmOverwriteFile(requirementspsd1Path)) {
-            await fse.writeFile(requirementspsd1Path, requirementspsd1);
+            if (majorVersion !== undefined) {
+                await fse.writeFile(requirementspsd1Path, requirementspsd1(majorVersion));
+            } else {
+                await fse.writeFile(requirementspsd1Path, requirementspsd1Offine);
+            }
         }
+    }
+
+    private async tryGetLatestAzModuleMajorVersion(progress: Progress<{ message?: string | undefined; increment?: number | undefined }>): Promise<number | undefined> {
+        progress.report({
+            message: localize('connecting', 'Connecting to PowerShell Gallery...')
+        });
+
+        try {
+            const xmlResult: string = await this.getPSGalleryAzModuleInfo();
+            const versionResult: string = await this.parseLatestAzModuleVersion(xmlResult);
+            const [major]: string[] = versionResult.split('.');
+            return parseInt(major);
+        } catch {
+            return undefined;
+        }
+    }
+
+    private async getPSGalleryAzModuleInfo(): Promise<string> {
+        const request: requestUtils.Request = await requestUtils.getDefaultRequest(this.azModuleGalleryUrl, undefined, 'GET');
+        return await requestUtils.sendRequest(request);
+    }
+
+    private async parseLatestAzModuleVersion(azModuleInfo: string): Promise<string> {
+        const moduleInfo: string = await new Promise((
+            resolve: (ret: string) => void,
+            // tslint:disable-next-line:no-any
+            rejects: (reason: any) => void): void => {
+            // tslint:disable-next-line:no-any
+            xml2js.parseString(azModuleInfo, { explicitArray: false }, (err: any, result: string): void => {
+                if (err) {
+                    rejects(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+
+        // tslint:disable-next-line:no-string-literal no-unsafe-any
+        if (moduleInfo['feed'] && moduleInfo['feed']['entry'] && Array.isArray(moduleInfo['feed']['entry'])) {
+            // tslint:disable-next-line:no-string-literal no-unsafe-any
+            const releasedVersions: string[] = moduleInfo['feed']['entry']
+                // tslint:disable-next-line:no-string-literal no-unsafe-any
+                .filter(entry => entry['m:properties']['d:IsPrerelease']['_'] === 'false')
+                // tslint:disable-next-line:no-string-literal no-unsafe-any
+                .map(entry => entry['m:properties']['d:Version']);
+
+            // Select the latest version
+            if (releasedVersions.length > 0) {
+                const lastIndex: number = releasedVersions.length - 1;
+                return releasedVersions[lastIndex];
+            }
+        }
+
+        // If no version is found, throw exception
+        throw new Error(`Failed to parse latest Az module version ${azModuleInfo}`);
     }
 }
