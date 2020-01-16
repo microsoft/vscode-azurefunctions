71a1c82691e25dcb38b8f5f5283e4cb0cb6c2396
Eric Jizba <erijiz@microsoft.com>
2020-01-16 13:40:23 -0800

    Add browse website

---
+++ src/commands/browseWebsite.ts
@@ -0,0 +1,18 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { IActionContext } from 'vscode-azureextensionui';
+import { ext } from '../extensionVariables';
+import { ProductionSlotTreeItem } from '../tree/ProductionSlotTreeItem';
+import { SlotTreeItemBase } from '../tree/SlotTreeItemBase';
+import { openUrl } from '../utils/openUrl';
+
+export async function browseWebsite(context: IActionContext, node?: SlotTreeItemBase): Promise<void> {
+    if (!node) {
+        node = await ext.tree.showTreeItemPicker<SlotTreeItemBase>(ProductionSlotTreeItem.contextValue, context);
+    }
+
+    await openUrl(node.root.client.defaultHostUrl);
+}
