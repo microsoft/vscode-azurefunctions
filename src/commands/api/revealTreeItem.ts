66379494501e594132a240b46b39588f54cf9ea4
Eric Jizba <erijiz@microsoft.com>
2020-02-03 10:03:04 -0800

    Add `revealTreeItem` api

---
+++ src/commands/api/revealTreeItem.ts
@@ -0,0 +1,16 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { AzExtTreeItem, callWithTelemetryAndErrorHandling, IActionContext } from "vscode-azureextensionui";
+import { ext } from "../../extensionVariables";
+
+export async function revealTreeItem(resourceId: string): Promise<void> {
+    return await callWithTelemetryAndErrorHandling('api.revealTreeItem', async (context: IActionContext) => {
+        const node: AzExtTreeItem | undefined = await ext.tree.findTreeItem(resourceId, { ...context, loadAll: true });
+        if (node) {
+            await ext.treeView.reveal(node, { select: true, focus: true, expand: true });
+        }
+    });
+}
--- src/extensionVariables.ts
+++ src/extensionVariables.ts
@@ -3,8 +3,8 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { ExtensionContext } from "vscode";
-import { AzExtTreeDataProvider, IAzExtOutputChannel, IAzureUserInput, ITelemetryReporter } from "vscode-azureextensionui";
+import { ExtensionContext, TreeView } from "vscode";
+import { AzExtTreeDataProvider, AzExtTreeItem, IAzExtOutputChannel, IAzureUserInput, ITelemetryReporter } from "vscode-azureextensionui";
 import { func } from "./constants";
 import { CentralTemplateProvider } from "./templates/CentralTemplateProvider";
 import { AzureAccountTreeItemWithProjects } from "./tree/AzureAccountTreeItemWithProjects";
@@ -15,6 +15,7 @@ import { AzureAccountTreeItemWithProjects } from "./tree/AzureAccountTreeItemWit
 export namespace ext {
     export let context: ExtensionContext;
     export let tree: AzExtTreeDataProvider;
+    export let treeView: TreeView<AzExtTreeItem>;
     export let azureAccountTreeItem: AzureAccountTreeItemWithProjects;
     export let outputChannel: IAzExtOutputChannel;
     export let ui: IAzureUserInput;
