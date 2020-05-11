99977fe388e3a55c1e5305168f3eabd9229b73e8
Eric Jizba <erijiz@microsoft.com>
2020-05-11 15:36:38 -0700

    fix tests

--- test/api.test.ts
+++ test/api.test.ts
@@ -7,8 +7,7 @@ import * as path from 'path';
 import { Extension, extensions } from "vscode";
 // tslint:disable-next-line: no-submodule-imports
 import { AzureExtensionApiProvider } from "vscode-azureextensionui/api";
-import { nonNullValue } from '../extension.bundle';
-import { ProjectLanguage } from '../src/constants';
+import { nonNullValue, ProjectLanguage } from '../extension.bundle';
 import { AzureFunctionsExtensionApi } from '../src/vscode-azurefunctions.api';
 import { testFolderPath, testUserInput } from './global.test';
 import { getJavaScriptValidateOptions, IValidateProjectOptions, validateProject } from './project/validateProject';
