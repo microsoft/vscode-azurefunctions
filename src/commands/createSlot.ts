cf76c429b29896d19b975fab73cf31eadce52a66
Eric Jizba <erijiz@microsoft.com>
2020-05-13 11:51:52 -0700

    Clean up output when creating function app/slot

--- src/commands/createSlot.ts
+++ src/commands/createSlot.ts
@@ -7,14 +7,14 @@ import { IActionContext } from 'vscode-azureextensionui';
 import { ext } from '../extensionVariables';
 import { SlotsTreeItem } from '../tree/SlotsTreeItem';
 import { SlotTreeItem } from '../tree/SlotTreeItem';
+import { ISiteCreatedOptions } from './createFunctionApp/showSiteCreated';
 
 export async function createSlot(context: IActionContext, node?: SlotsTreeItem): Promise<string> {
     if (!node) {
         node = await ext.tree.showTreeItemPicker<SlotsTreeItem>(SlotsTreeItem.contextValue, context);
     }
 
+    (<ISiteCreatedOptions>context).showCreatedNotification = true;
     const slotNode: SlotTreeItem = await node.createChild(context);
-    slotNode.showCreatedOutput();
-
     return slotNode.fullId;
 }
