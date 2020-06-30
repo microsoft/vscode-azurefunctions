eddcb36c3e31efafda176cd88b41a999368ac4da
Eric Jizba <erijiz@microsoft.com>
2020-06-30 11:19:24 -0700

    Refresh tree after swapping slot

--- src/commands/swapSlot.ts
+++ src/commands/swapSlot.ts
@@ -15,4 +15,5 @@ export async function swapSlot(context: IActionContext, sourceSlotNode?: SlotTre
 
     const deploymentSlots: SlotTreeItem[] = <SlotTreeItem[]>await sourceSlotNode.parent.getCachedChildren(context);
     await appservice.swapSlot(sourceSlotNode, deploymentSlots);
+    await sourceSlotNode.parent.parent.refresh();
 }
