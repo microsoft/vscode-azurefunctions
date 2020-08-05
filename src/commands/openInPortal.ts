7366769e786b7fe8c7ef67dbc4ed3ac5b4d072bc
Eric Jizba <erijiz@microsoft.com>
2020-08-05 14:02:33 -0700

    Fix open in portal for deployment node

--- src/commands/openInPortal.ts
+++ src/commands/openInPortal.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { AzureTreeItem, IActionContext } from 'vscode-azureextensionui';
+import { AzureTreeItem, IActionContext, openInPortal as uiOpenInPortal } from 'vscode-azureextensionui';
 import { ext } from '../extensionVariables';
 import { ProductionSlotTreeItem } from '../tree/ProductionSlotTreeItem';
 
@@ -12,5 +12,5 @@ export async function openInPortal(context: IActionContext, node?: AzureTreeItem
         node = await ext.tree.showTreeItemPicker<AzureTreeItem>(ProductionSlotTreeItem.contextValue, context);
     }
 
-    await node.openInPortal();
+    await uiOpenInPortal(node, node.fullId);
 }
