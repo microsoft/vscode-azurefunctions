e3186d7e335e2dd68baeadb153cf8ed31cb77490
Eric Jizba <erijiz@microsoft.com>
2020-09-29 17:44:48 -0700

    Support comments in local.settings.json

---
+++ test/parseJson.test.ts
@@ -0,0 +1,57 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as assert from 'assert';
+import { getLineAndColumnFromOffset } from '../extension.bundle';
+import { parseJson } from '../src/utils/parseJson';
+
+suite('parseJson', () => {
+    test('Valid json', () => {
+        const data: string = '{"a": "1"}';
+        assert.deepStrictEqual(parseJson(data), { a: '1' });
+    });
+
+    test('Trailing comma', () => {
+        const data: string = '{"a": "1",}';
+        assert.deepStrictEqual(parseJson(data), { a: '1' });
+    });
+
+    test('With comment', () => {
+        const data: string = '{"a": /*comment*/"1"}';
+        assert.deepStrictEqual(parseJson(data), { a: '1' });
+    });
+
+    test('Invalid json', () => {
+        const data: string = '{"a": "1}';
+        assert.throws(() => parseJson(data));
+    });
+});
+
+suite('getLineAndColumnFromOffset', () => {
+    const data: string = `{
+    "version": "2.0",
+    "logging": {
+        "applicationInsights": {
+            "samplingExcludedTypes": "Request",
+            "samplingSettings": {
+                "isEnabled": true
+            }
+        }
+    }
+}`;
+
+    test('First character', () => {
+        assert.deepStrictEqual(getLineAndColumnFromOffset(data, 1), [1, 1]);
+    });
+    test('Middle of random line', () => {
+        assert.deepStrictEqual(getLineAndColumnFromOffset(data, 28), [3, 4]);
+    });
+    test('End of random line', () => {
+        assert.deepStrictEqual(getLineAndColumnFromOffset(data, 74), [4, 33]);
+    });
+    test('Last character', () => {
+        assert.deepStrictEqual(getLineAndColumnFromOffset(data, 221), [11, 1]);
+    });
+});
