ada7b568531dd0435f4fc72f90713f5b8a1541b4
Varad Meru [gmail] <varad.meru@gmail.com>
2020-10-25 22:20:59 -0700

    Adding python requirements update and error reporting

--- src/commands/createFunction/openAPISteps/OpenAPICreateStep.ts
+++ src/commands/createFunction/openAPISteps/OpenAPICreateStep.ts
@@ -3,14 +3,15 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import * as fse from 'fs-extra';
 import * as path from 'path';
 import { ProgressLocation, Uri, window } from "vscode";
 import { AzureWizardExecuteStep, DialogResponses, IActionContext } from 'vscode-azureextensionui';
-import { ProjectLanguage } from "../../../constants";
+import { ProjectLanguage, requirementsFileName } from "../../../constants";
 import { ext } from "../../../extensionVariables";
 import { localize } from "../../../localize";
 import { cpUtils } from "../../../utils/cpUtils";
-import { confirmEditJsonFile } from '../../../utils/fs';
+import { confirmEditJsonFile, confirmOverwriteFile } from '../../../utils/fs';
 import { nonNullProp } from '../../../utils/nonNull';
 import { openUrl } from '../../../utils/openUrl';
 import { IJavaProjectWizardContext } from '../../createNewProject/javaSteps/IJavaProjectWizardContext';
@@ -29,46 +30,69 @@ export class OpenAPICreateStep extends AzureWizardExecuteStep<IFunctionWizardCon
         const uris: Uri[] = nonNullProp(wizardContext, 'openApiSpecificationFile');
         const uri: Uri = uris[0];
         const args: string[] = [];
-
-        args.push(`--input-file:${cpUtils.wrapArgInQuotes(uri.fsPath)}`);
-        args.push(`--version:3.0.6320`);
+        const pluginRunPrefix: string = 'azure-functions';
+        let pluginRun: string = '';
 
         switch (wizardContext.language) {
             case ProjectLanguage.TypeScript:
-                args.push('--azure-functions-typescript');
+                pluginRun = 'typescript';
                 args.push('--no-namespace-folders:True');
                 break;
             case ProjectLanguage.CSharp:
+                pluginRun = 'csharp';
                 args.push(`--namespace:${nonNullProp(wizardContext, 'namespace')}`);
-                args.push('--azure-functions-csharp');
                 break;
             case ProjectLanguage.Java:
+                pluginRun = 'java';
                 args.push(`--namespace:${nonNullProp(wizardContext, 'javaPackageName')}`);
-                args.push('--azure-functions-java');
                 break;
             case ProjectLanguage.Python:
-                args.push('--azure-functions-python');
-                args.push('--no-namespace-folders:True');
+                pluginRun = 'python';
                 args.push('--no-async');
+                args.push('--no-namespace-folders:True');
                 break;
             default:
                 throw new Error(localize('notSupported', 'Not a supported language. We currently support C#, Java, Python, and Typescript'));
-                break;
         }
 
-        args.push('--generate-metadata:false');
+        args.push(`--${pluginRunPrefix}-${pluginRun}`);
+        args.push(`--input-file:${cpUtils.wrapArgInQuotes(uri.fsPath)}`);
         args.push(`--output-folder:${cpUtils.wrapArgInQuotes(wizardContext.projectPath)}`);
+        args.push('--generate-metadata:false');
+        args.push(`--version:3.0.6320`);
 
         ext.outputChannel.appendLog(localize('statutoryWarning', 'Using the plugin could overwrite your custom changes to the functions.\nIf autorest fails, you can run the script on your command-line, or try resetting autorest (autorest --reset) and try again.'));
-        const title: string = localize('generatingFunctions', 'Generating from OpenAPI...Check [output window](command:{0}) for status.', ext.prefix + '.showOutputChannel');
+        ext.outputChannel.appendLog(localize('urlsForIssues', 'If there are any issues, please visit https://aka.ms/stencil for usage information or report issues at https://aka.ms/stencil/issues.'));
+        const title: string = localize('generatingFunctions', 'Generating from OpenAPI Specification...Check [output window](command:{0}) for status.', ext.prefix + '.showOutputChannel');
 
         await window.withProgress({ location: ProgressLocation.Notification, title }, async () => {
-            await cpUtils.executeCommand(ext.outputChannel, undefined, 'autorest', ...args);
+            try {
+                await cpUtils.executeCommand(ext.outputChannel, undefined, 'autorest', ...args);
+            } catch (error) {
+                const message: string = localize('autorestNotRunSuccessfully',
+                    `Failed to run "autorest.${pluginRunPrefix}-${pluginRun}" | Click "Report an Issue" to report the error.`);
+                if (!wizardContext.errorHandling.suppressDisplay) {
+                    window.showErrorMessage(message, DialogResponses.reportAnIssue).then(async result => {
+                        if (result === DialogResponses.reportAnIssue) {
+                            await openUrl('https://aka.ms/stencil/issues');
+                        }
+                    });
+                    wizardContext.errorHandling.suppressDisplay = true;
+                }
+
+                throw new Error(message);
+            }
         });
 
-        if (wizardContext.language === ProjectLanguage.TypeScript) {
-            // Change the package.json only when the autorest successfully ran.
-            await addAutorestSpecificTypescriptDependencies(wizardContext);
+        switch (wizardContext.language) {
+            case ProjectLanguage.TypeScript:
+                await addAutorestSpecificTypescriptDependencies(wizardContext);
+                break;
+            case ProjectLanguage.Python:
+                await addAutorestSpecificPythonDependencies(wizardContext);
+                break;
+            default:
+                break;
         }
     }
 
@@ -108,4 +132,34 @@ async function addAutorestSpecificTypescriptDependencies(context: IFunctionWizar
         }
         return data;
     });
+}
+
+async function addAutorestSpecificPythonDependencies(context: IFunctionWizardContext): Promise<void> {
+
+    let oldRequirements: string = '';
+    let newRequirements: string = '';
+    const requirementsPath: string = path.join(context.projectPath, requirementsFileName);
+
+    // Current list of dependencies to be added to support stencil generated projects.
+    const dependenciesToAddArray: string[] = ['msrest'];
+
+    // Some custom requirements might not have newline character added already.
+    const dependenciesToAdd: string = `\n${dependenciesToAddArray.join('\n')}\n`;
+
+    if (await fse.pathExists(requirementsPath)) {
+        try {
+            oldRequirements = (await fse.readFile(requirementsPath)).toString();
+            newRequirements = oldRequirements.concat(dependenciesToAdd);
+        } catch (error) {
+            // If we failed to parse or edit the existing file, just ask to overwrite the file completely
+            if (await confirmOverwriteFile(requirementsPath)) {
+                newRequirements = dependenciesToAdd;
+            } else {
+                return;
+            }
+        }
+    } else {
+        newRequirements = dependenciesToAdd;
+    }
+    await fse.writeFile(requirementsPath, newRequirements);
 }
