010719623834e5c260a98d6e570f1c9a540833c0
Eric Jizba <erijiz@microsoft.com>
2020-12-09 13:15:16 -0800

    Debugging with .NET 5 (#2579)

--- src/utils/windowsProcessTree.ts
+++ src/utils/windowsProcessTree.ts
@@ -16,14 +16,36 @@ export function getWindowsProcessTree(): IWindowsProcessTree {
     return windowsProcessTree;
 }
 
-export interface IWindowsProcessTree {
-    getProcessTree(rootPid: number, callback: (tree: IProcessTreeNode | undefined) => void): void;
+// https://github.com/microsoft/vscode-windows-process-tree/blob/b7efc9fb4567d552ef95c7449058b6f634a82df8/typings/windows-process-tree.d.ts
+
+export enum ProcessDataFlag {
+    None = 0,
+    Memory = 1,
+    CommandLine = 2
 }
 
-export interface IProcessTreeNode {
+export interface IProcessInfo {
     pid: number;
+    ppid: number;
     name: string;
+
+    /**
+     * The working set size of the process, in bytes.
+     */
     memory?: number;
+
+    /**
+     * The string returned is at most 512 chars, strings exceeding this length are truncated.
+     */
     commandLine?: string;
-    children: IProcessTreeNode[];
+}
+
+export interface IWindowsProcessTree {
+    /**
+     * Returns a list of processes containing the rootPid process and all of its descendants.
+     * @param rootPid - The pid of the process of interest.
+     * @param callback - The callback to use with the returned set of processes.
+     * @param flags - The flags for what process data should be included.
+     */
+    getProcessList(rootPid: number, callback: (processList: IProcessInfo[]) => void, flags?: ProcessDataFlag): void;
 }
