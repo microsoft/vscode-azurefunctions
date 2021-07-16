ea24cf5622df441a5d6d628277da90b6226c12f2
Chloe Quijano <61916412+ChloeQuijano@users.noreply.github.com>
2021-07-16 12:16:36 -0400

    Cleaned up docker prompt file  (#4)
    * cleaned up nested if statements in docker prompt file
    
    * removed ': string" type declaration
    
    * removed inline strings and replaced with constants
    
    * changed dev container download call
    
    * fixed equality operator
    
    Co-authored-by: Chloe Quijano <t-cquijano@microsoft.com>

--- src/commands/dockersupport/localDockerSupport.ts
+++ src/commands/dockersupport/localDockerSupport.ts
@@ -6,6 +6,9 @@ import { requestUtils } from "../../utils/requestUtils";
 import { prompt, prompt2 } from "./askDockerStep";
 import { validateDockerInstalled } from "./validateDockerInstalled";
 
+export const DOCKER_PROMPT_YES = "yes";
+export const PLATFORM_WIN32 = "win32";
+
 /**
  * Main Function Called to initialize the Docker flow with cloning Function App project locally from VS Code Extension
  * @param context - behavour of action
@@ -14,51 +17,39 @@ import { validateDockerInstalled } from "./validateDockerInstalled";
  * @param node - Function App Project
  */
 export async function localDockerPrompt(context: IActionContext, devContainerFolderPathUri: vscode.Uri, node?: SlotTreeItemBase, devContainerName?: string): Promise<void> {
-    if (node) {
-        // check if language is supported for dev container
-        if (devContainerName) {
-            // check if the function app is in Linux
-            if (node.site.reserved) {
-                // asks if the user wants to use Docker for initializing the project locally
-                const useDocker: string = await prompt(context);
-                if (useDocker === "yes") {
-                    // download dev container
-                    await requestUtils.downloadFile(
-                        `https://raw.githubusercontent.com/microsoft/vscode-dev-containers/master/containers/${devContainerName}/.devcontainer/devcontainer.json`,
-                        vscode.Uri.joinPath(devContainerFolderPathUri, 'devcontainer.json').fsPath
-                    );
-                    // download docker file
-                    await requestUtils.downloadFile(
-                        `https://raw.githubusercontent.com/microsoft/vscode-dev-containers/master/containers/${devContainerName}/.devcontainer/Dockerfile`,
-                        vscode.Uri.joinPath(devContainerFolderPathUri, 'Dockerfile').fsPath
-                    );
-                    // external - check if Docker is installed, Remote Development extension AND Docker Extension
-                    if (!validateDockerInstalled()) {
-                        // if Docker is not downloaded - ask the user if they'd like to download Docker
-                        const downloadDocker: string = await prompt2(context);
-                        if (downloadDocker === "yes") {
-                            // Check if Operating System is Windows
-                            if (process.platform == "win32") {
-                                // Download Docker with an MSI package
-                            } else {
-                                // Not windows: display link to download docker externally from Docker documentation
-                                void vscode.window.showInformationMessage(localize('downloadDocker', 'Check the (Docker documentation)[https://docs.docker.com/get-docker/] to download Docker for your system'));
-                            }
-                        } else {
-                            void vscode.window.showInformationMessage(localize('noDocker', 'Continuing without the use of Docker as user requested'));
-                        }
+    if (node && devContainerName && node.site.reserved) {
+        // asks if the user wants to use Docker for initializing the project locally
+        if (await prompt(context) === DOCKER_PROMPT_YES) {
+            await downloadLocalDevFiles(devContainerFolderPathUri, devContainerName);
+            // external - check if Docker is installed, Remote Development extension AND Docker Extension
+            if (!validateDockerInstalled()) {
+                // if Docker is not downloaded - ask the user if they'd like to download Docker
+                if (await prompt2(context) === DOCKER_PROMPT_YES) {
+                    // Check if Operating System is Windows
+                    if (process.platform.toLowerCase() === PLATFORM_WIN32) {
+                        // Download Docker with an MSI package
+                    } else {
+                        // Not windows: display link to download docker externally from Docker documentation
+                        void vscode.window.showInformationMessage(localize('downloadDocker', 'Check the (Docker documentation)[https://docs.docker.com/get-docker/] to download Docker for your system'));
                     }
                 } else {
                     void vscode.window.showInformationMessage(localize('noDocker', 'Continuing without the use of Docker as user requested'));
                 }
-            } else {
-                void vscode.window.showInformationMessage(localize('projectError', 'Function App is not Linux. Continuing without the use of Docker'));
             }
         } else {
-            void vscode.window.showInformationMessage(localize('runtimeError', 'Language not supported. Continuing without the use of Docker'));
+            void vscode.window.showInformationMessage(localize('noDocker', 'Continuing without the use of Docker as user requested'));
         }
     } else {
-        void vscode.window.showInformationMessage(localize('noNode', 'Node is undefined. Continuing without the use of Docker'));
+        void vscode.window.showInformationMessage(localize('unableDocker', 'Initializing project without the use of Docker'));
     }
+}
 
+export async function downloadLocalDevFiles(devContainerFolderPathUri: vscode.Uri, devContainerName?: string): Promise<void> {
+    const downloadDevContainerJson = requestUtils.downloadFile(`https://raw.githubusercontent.com/microsoft/vscode-dev-containers/master/containers/${devContainerName}/.devcontainer/devcontainer.json`, vscode.Uri.joinPath(devContainerFolderPathUri, 'devcontainer.json').fsPath);
+    const downloadDevContainerDockerfile = requestUtils.downloadFile(`https://raw.githubusercontent.com/microsoft/vscode-dev-containers/master/containers/${devContainerName}/.devcontainer/Dockerfile`, vscode.Uri.joinPath(devContainerFolderPathUri, 'Dockerfile').fsPath);
+
+    await Promise.all([
+        downloadDevContainerJson,
+        downloadDevContainerDockerfile
+    ]);
 }
