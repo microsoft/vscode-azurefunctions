14dcb9b1b6b11b9481e2be9c289a45f0ff583d05
Alex Weininger <alex.weininger@live.com>
2022-01-04 13:04:15 -0800

    Handle multiple target frameworks when creating function via API (#3031)

--- src/commands/createNewProject/dotnetSteps/DotnetRuntimeStep.ts
+++ src/commands/createNewProject/dotnetSteps/DotnetRuntimeStep.ts
@@ -15,11 +15,13 @@ import { IProjectWizardContext } from "../IProjectWizardContext";
 export class DotnetRuntimeStep extends AzureWizardPromptStep<IProjectWizardContext> {
     public static async createStep(context: IProjectWizardContext): Promise<DotnetRuntimeStep> {
         if (context.targetFramework) {
+            context.targetFramework = typeof context.targetFramework === 'string' ? [context.targetFramework] : context.targetFramework;
             const runtimes = await getRuntimes(context);
             // if a targetFramework was provided from createNewProject
-            const workerRuntime = runtimes.find(runtime => runtime.targetFramework === context.targetFramework);
+            const workerRuntime = runtimes.find(runtime => context.targetFramework?.includes(runtime.targetFramework));
             if (!workerRuntime) {
-                throw new Error(localize('unknownFramework', 'Unrecognized target framework "{0}". Available frameworks: {1}.', context.targetFramework,
+                throw new Error(localize('unknownFramework', 'Unrecognized target frameworks: "{0}". Available frameworks: {1}.',
+                    context.targetFramework.map(tf => `"${tf}"`).join(', '),
                     runtimes.map(rt => `"${rt.targetFramework}"`).join(', ')));
             }
             setWorkerRuntime(context, workerRuntime);
