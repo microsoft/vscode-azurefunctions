ad8075368ace2e6678d4b1fc5c04257de05b2a00
Alex Weininger <alex.weininger@live.com>
2022-05-04 12:47:29 -0700

    Hide openInPortal command from palette (#3153)

--- src/commands/openInPortal.ts
+++ src/commands/openInPortal.ts
@@ -5,17 +5,7 @@
 
 import { openInPortal as uiOpenInPortal } from '@microsoft/vscode-azext-azureutils';
 import { AzExtTreeItem, IActionContext, nonNullProp } from '@microsoft/vscode-azext-utils';
-import { functionFilter } from '../constants';
-import { ext } from '../extensionVariables';
-import { ResolvedFunctionAppResource } from '../tree/ResolvedFunctionAppResource';
 
-export async function openDeploymentInPortal(context: IActionContext, node?: AzExtTreeItem): Promise<void> {
-    if (!node) {
-        node = await ext.rgApi.pickAppResource<AzExtTreeItem>(context, {
-            filter: functionFilter,
-            expectedChildContextValue: new RegExp(ResolvedFunctionAppResource.productionContextValue)
-        });
-    }
-
+export async function openDeploymentInPortal(_context: IActionContext, node: AzExtTreeItem): Promise<void> {
     await uiOpenInPortal(node, `${nonNullProp(node, 'parent').parent?.id}/Deployments/${nonNullProp(node, 'id')}`);
 }
