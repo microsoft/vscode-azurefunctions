c0171ad2b8933765e0e049702bf697f697bdcaf5
Alex Weininger <alex.weininger@live.com>
2022-10-19 14:17:27 -0700

    Revert "Use `CompatibilityAppResourceResolverBase`"
    This reverts commit 396343e246d11d2f32a3dbcb0f96baec74f911ca.

--- src/FunctionAppResolver.ts
+++ src/FunctionAppResolver.ts
@@ -1,10 +1,10 @@
 import { getResourceGroupFromId } from "@microsoft/vscode-azext-azureutils";
-import { callWithTelemetryAndErrorHandling, CompatabilityAppResourceResolverBase, IActionContext, ISubscriptionContext, nonNullProp } from "@microsoft/vscode-azext-utils";
-import { AppResource } from "@microsoft/vscode-azext-utils/hostapi";
+import { callWithTelemetryAndErrorHandling, IActionContext, ISubscriptionContext, nonNullProp } from "@microsoft/vscode-azext-utils";
+import { AppResource, AppResourceResolver } from "@microsoft/vscode-azext-utils/hostapi";
 import { ResolvedFunctionAppResource } from "./tree/ResolvedFunctionAppResource";
 import { createWebSiteClient } from "./utils/azureClients";
 
-export class FunctionAppResolver extends CompatabilityAppResourceResolverBase {
+export class FunctionAppResolver implements AppResourceResolver {
     public async resolveResource(subContext: ISubscriptionContext, resource: AppResource): Promise<ResolvedFunctionAppResource | undefined> {
         return await callWithTelemetryAndErrorHandling('resolveResource', async (context: IActionContext) => {
             const client = await createWebSiteClient({ ...context, ...subContext });
