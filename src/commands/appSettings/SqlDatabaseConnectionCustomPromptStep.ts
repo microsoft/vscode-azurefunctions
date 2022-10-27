742f7c73bbe520548b38806fea80a2d44788361f
MicroFish91 <mwfisher91@gmail.com>
2022-10-26 17:01:16 -0700

    update microsoft copyright

--- src/commands/appSettings/SqlDatabaseConnectionCustomPromptStep.ts
+++ src/commands/appSettings/SqlDatabaseConnectionCustomPromptStep.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import { AzureWizardPromptStep } from '@microsoft/vscode-azext-utils';
 import { ConnectionType } from '../../constants';
 import { invalidLength, localize } from '../../localize';
