24d9244fff4d6ddb5c70ac1f01a4149f53901fc6
Megan Mott <59709511+motm32@users.noreply.github.com>
2022-11-10 11:16:27 -0800

    Remove !== undefined
    Co-authored-by: Alex Weininger <alex.weininger@live.com>

--- src/commands/createFunctionApp/FunctionAppCreateStep.ts
+++ src/commands/createFunctionApp/FunctionAppCreateStep.ts
@@ -81,7 +81,7 @@ export class FunctionAppCreateStep extends AzureWizardExecuteStep<IFunctionAppWi
         }
 
         // Always on setting added for App Service plans excluding the free tier https://github.com/microsoft/vscode-azurefunctions/issues/3037
-        if (context.plan?.sku?.family !== undefined) {
+        if (context.plan?.sku?.family) {
             const isNotFree = context.plan.sku.family.toLowerCase() !== 'u';
             const isNotElasticPremium = context.plan.sku.family.toLowerCase() !== 'ep';
             const isNotConsumption: boolean = context.plan.sku.family.toLowerCase() !== 'y';
