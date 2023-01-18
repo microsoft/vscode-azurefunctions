fc19934e72d802f8bd39f2c7395522e6a1afa856
MicroFish91 <mwfisher91@gmail.com>
2023-01-17 21:59:20 -0800

    misc

--- src/FuncVersion.ts
+++ src/FuncVersion.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { IActionContext, IAzureQuickPickItem, IAzureQuickPickOptions } from '@microsoft/vscode-azext-utils';
-import { localize, recommended } from './localize';
+import { localize } from './localize';
 import { openUrl } from './utils/openUrl';
 
 export enum FuncVersion {
@@ -17,6 +17,7 @@ export enum FuncVersion {
 export const latestGAVersion: FuncVersion = FuncVersion.v4;
 
 export async function promptForFuncVersion(context: IActionContext, message?: string): Promise<FuncVersion> {
+    const recommended: string = localize('recommended', '(Recommended)');
     let picks: IAzureQuickPickItem<FuncVersion | undefined>[] = [
         { label: 'Azure Functions v4', description: recommended, data: FuncVersion.v4 },
         { label: 'Azure Functions v3', data: FuncVersion.v3 },
