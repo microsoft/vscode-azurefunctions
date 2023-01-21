4cf88064ceeb6234d99d2f1f23c5650441306a7b
MicroFish91 <mwfisher91@gmail.com>
2023-01-20 21:11:41 -0800

    add shouldValidate comment

--- src/commands/deploy/shouldValidateConnection.ts
+++ src/commands/deploy/shouldValidateConnection.ts
@@ -13,6 +13,12 @@ export interface IShouldValidateConnection {
     shouldValidateSqlDb: boolean;
 }
 
+/*
+ * In general, most of the logic here boils down to the following cases.
+ * Case 1: No remote connection exists for a required resource.  shouldValidate returns true.
+ * Case 2: A remote connection already exists, but there is also a valid local settings connection detected in 'local.settings.json' that could replace it.
+ * In this case, prompt the user to determine if they would like to overwrite.  If yes, shouldValidate returns true.
+ */
 export async function shouldValidateConnections(context: IActionContext, durableStorageType: DurableBackendValues | undefined, client: SiteClient, projectPath: string): Promise<IShouldValidateConnection> {
     const app: StringDictionary = await client.listApplicationSettings();
     const remoteStorageConnection: string | undefined = app?.properties?.[ConnectionKey.Storage];
