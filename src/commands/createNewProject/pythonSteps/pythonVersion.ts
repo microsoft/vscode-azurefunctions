2caacd0cdf517ff7c5b21983469c70230d69fa80
Megan Mott <59709511+motm32@users.noreply.github.com>
2023-01-27 18:45:33 -0500

    Remove 3.6 (#3533)

--- src/commands/createNewProject/pythonSteps/pythonVersion.ts
+++ src/commands/createNewProject/pythonSteps/pythonVersion.ts
@@ -32,7 +32,6 @@ export async function getSupportedPythonVersions(context: IActionContext, funcVe
     const result: string[] = [];
 
     const versionInfo: [string, string][] = [
-        ['2.0.0', '3.6'],
         ['2.7.1846', '3.7'],
         ['3.0.2245', '3.8'],
         ['3.0.3216', '3.9'],
