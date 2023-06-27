9663332fb456b2a62ac2a2e5f095c19441940980
Nathan Turinski <naturins@microsoft.com>
2023-06-27 15:16:12 -0700

    Fix build error

--- src/commands/appSettings/connectionSettings/eventHubs/IEventHubsConnectionWizardContext.ts
+++ src/commands/appSettings/connectionSettings/eventHubs/IEventHubsConnectionWizardContext.ts
@@ -3,17 +3,17 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import type { ResourceManagementModels } from "@azure/arm-resources";
-import { ISubscriptionContext } from "@microsoft/vscode-azext-utils";
+import type { ResourceGroup } from "@azure/arm-resources";
 import { EventHubsConnectionTypeValues, StorageConnectionTypeValues } from "../../../../constants";
+import { IEventHubWizardContext } from "../../../addBinding/settingSteps/eventHub/IEventHubWizardContext";
 import { ISetConnectionSettingContext } from "../ISetConnectionSettingContext";
 
-export interface IEventHubsConnectionWizardContext extends ISetConnectionSettingContext, Partial<ISubscriptionContext> {
-    resourceGroup?: ResourceManagementModels.ResourceGroup;
+export interface IEventHubsConnectionWizardContext extends IEventHubWizardContext, ISetConnectionSettingContext {
+    resourceGroup?: ResourceGroup;
 
     // Connection Types
     azureWebJobsStorageType?: StorageConnectionTypeValues;
     eventHubsConnectionType?: EventHubsConnectionTypeValues;
 
     // Netherite uses all of the eventhub namespace settings in IEventHubWizardContext
-}
+}``
