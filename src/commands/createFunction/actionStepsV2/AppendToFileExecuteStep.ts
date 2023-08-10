27634bc5d533f0d41819aa9a9d080ad1ceb7fefe
Nathan Turinski <naturins@microsoft.com>
2023-08-10 14:41:14 -0700

    Use AzExtFsExtra.appendFile

--- src/commands/createFunction/actionStepsV2/AppendToFileExecuteStep.ts
+++ src/commands/createFunction/actionStepsV2/AppendToFileExecuteStep.ts
@@ -9,12 +9,9 @@ import { WriteToFileExecuteStep } from "./WriteToFileExecuteStep";
 
 export class AppendToFileExecuteStep<T extends FunctionV2WizardContext> extends WriteToFileExecuteStep<T> {
     protected async writeToFile(context: T, filePath: string): Promise<void> {
-        const existingContent = await AzExtFsExtra.readFile(filePath);
         const sourceKey = nonNullProp(this.action, 'source');
         const source = context[sourceKey] as string;
 
-        // NOTE: AzExtFsExtra doesn't have fs-extra's handy appendFile() function.
-        // NOTE: We add two (end-of-)lines to ensure an empty line between functions definitions for function_body.
-        await AzExtFsExtra.writeFile(filePath, existingContent + '\r\n\r\n' + source);
+        await AzExtFsExtra.appendFile(filePath, source);
     }
 }
