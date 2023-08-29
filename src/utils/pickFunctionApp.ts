58588c19a7a560e795f54c8e8f7042c1ef4fef9b
Megan Mott <59709511+motm32@users.noreply.github.com>
2023-08-29 13:17:09 -0700

    Add Service Connector (#3790)
    * Add Service Connector
    
    * Requested changes and update package
    
    * Add name to activity log
    
    * Hide validate from the command palette
    
    * Update package and change create/delete name
    
    * one more change
    
    * Update dev and serviceconnector package

---
+++ src/utils/pickFunctionApp.ts
@@ -0,0 +1,15 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.md in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+import { ITreeItemPickerContext } from "@microsoft/vscode-azext-utils";
+import { functionFilter } from "../constants";
+import { ext } from "../extensionVariables";
+import { SlotTreeItem } from "../tree/SlotTreeItem";
+
+export async function pickFunctionApp(context: ITreeItemPickerContext): Promise<SlotTreeItem> {
+    return await ext.rgApi.pickAppResource<SlotTreeItem>(context, {
+        filter: functionFilter
+    });
+}
