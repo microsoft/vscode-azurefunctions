bb82cd77538c6e88f7b42445240b0f120f2e431b
Nathan Turinski <naturins@microsoft.com>
2023-09-11 15:35:22 -0700

    Fix build

--- src/utils/programmingModelUtils.ts
+++ src/utils/programmingModelUtils.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { nodeV4ModelVersion, ProjectLanguage } from "../constants";
+import { ProjectLanguage } from "../constants";
 
 export function isPythonV2Plus(language: string | undefined, model: number | undefined): boolean {
     return language === ProjectLanguage.Python && model !== undefined && model > 1;
@@ -14,5 +14,5 @@ export const nodeV4Suffix = '-4.x';
 export function isNodeV4Plus(context: { language?: ProjectLanguage | string, languageModel?: number }): boolean {
     const { language, languageModel } = context;
     return (language === ProjectLanguage.JavaScript || language === ProjectLanguage.TypeScript) &&
-        languageModel !== undefined && languageModel >= nodeV4ModelVersion;
+        languageModel !== undefined && languageModel > 4;
 }
