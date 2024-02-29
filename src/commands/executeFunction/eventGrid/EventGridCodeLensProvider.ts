2adbf3954245730762eb4349182ceb92a19cc8ee
hossam-nasr <hossam.nasr99@gmail.com>
2024-02-29 02:35:55 -0800

    add aka.ms link & add code lens at the bottom

--- src/commands/executeFunction/eventGrid/EventGridCodeLensProvider.ts
+++ src/commands/executeFunction/eventGrid/EventGridCodeLensProvider.ts
@@ -2,17 +2,21 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { CodeLens, Range, type CodeLensProvider } from 'vscode';
+import { CodeLens, Range, type CodeLensProvider, type TextDocument } from 'vscode';
 import { localize } from '../../../localize';
 
 export class EventGridCodeLensProvider implements CodeLensProvider {
-    public provideCodeLenses(): CodeLens[] {
-        const sendRequestLens = new CodeLens(new Range(0, 0, 0, 0));
-        sendRequestLens.command = {
+    public provideCodeLenses(document: TextDocument): CodeLens[] {
+        const firstLineLens = new CodeLens(new Range(0, 0, 0, 0));
+        const lastLine = document.lineAt(document.lineCount);
+        const lastLineLens = new CodeLens(new Range(lastLine.range.start, lastLine.range.end));
+
+        const command = {
             title: localize('saveSendRequest', 'Save and send request'),
             command: 'azureFunctions.eventGrid.sendMockRequest',
         };
+        firstLineLens.command = lastLineLens.command = command;
 
-        return [sendRequestLens];
+        return [firstLineLens, lastLineLens];
     }
 }
