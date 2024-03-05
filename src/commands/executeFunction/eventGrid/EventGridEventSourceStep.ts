1ceca9e74bbbbfaf0a28b74d7ace790c1aaae815
hossam-nasr <hossam.nasr99@gmail.com>
2024-03-05 01:44:55 -0800

    add licenses

--- src/commands/executeFunction/eventGrid/EventGridEventSourceStep.ts
+++ src/commands/executeFunction/eventGrid/EventGridEventSourceStep.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import { AzureWizardPromptStep, type IAzureQuickPickItem } from "@microsoft/vscode-azext-utils";
 import { localize } from "../../../localize";
 import { type ExecuteEventGridFunctionContext } from "./ExecuteEventGridFunctionContext";
--- src/commands/executeFunction/eventGrid/OpenEventGridFileStep.ts
+++ src/commands/executeFunction/eventGrid/OpenEventGridFileStep.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import { AzExtFsExtra, AzureWizardExecuteStep, nonNullProp } from "@microsoft/vscode-azext-utils";
 import * as os from 'os';
 import * as path from "path";
