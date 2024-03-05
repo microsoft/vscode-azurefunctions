b2029b426a245b4e060a4e2ab67bcaa03fb66fe1
hossam-nasr <hossam.nasr99@gmail.com>
2024-03-05 01:44:07 -0800

    remove last line codelens

--- src/commands/executeFunction/eventGrid/EventGridCodeLensProvider.ts
+++ src/commands/executeFunction/eventGrid/EventGridCodeLensProvider.ts
@@ -2,21 +2,18 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { CodeLens, Range, type CodeLensProvider, type TextDocument } from 'vscode';
+import { CodeLens, Range, type CodeLensProvider } from 'vscode';
 import { localize } from '../../../localize';
 
 export class EventGridCodeLensProvider implements CodeLensProvider {
-    public provideCodeLenses(document: TextDocument): CodeLens[] {
+    public provideCodeLenses(): CodeLens[] {
         const firstLineLens = new CodeLens(new Range(0, 0, 0, 0));
-        const lastLine = document.lineAt(document.lineCount - 1);
-        const lastLineLens = new CodeLens(new Range(lastLine.range.start, lastLine.range.end));
 
-        const command = {
-            title: localize('saveSendRequest', 'Save and execute'),
+        firstLineLens.command = {
+            title: localize('saveExecute', 'Save and execute'),
             command: 'azureFunctions.eventGrid.sendMockRequest',
         };
-        firstLineLens.command = lastLineLens.command = command;
 
-        return [firstLineLens, lastLineLens];
+        return [firstLineLens];
     }
 }
