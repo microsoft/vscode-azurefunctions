5c26d59e662be930da170bdf31f9b0fcca0be16f
Nathan Turinski <naturins@microsoft.com>
2024-05-16 19:14:13 -0700

    Remove preview flag for flex

--- src/commands/createFunctionApp/FunctionAppHostingPlanStep.ts
+++ src/commands/createFunctionApp/FunctionAppHostingPlanStep.ts
@@ -11,22 +11,17 @@ import { AzureWizardPromptStep, type IAzureQuickPickItem } from '@microsoft/vsco
 import { localize } from '../../localize';
 import { getRandomHexString } from '../../utils/fs';
 import { nonNullProp } from '../../utils/nonNull';
-import { getWorkspaceSetting } from '../../vsCodeConfig/settings';
 import { type IFunctionAppWizardContext } from './IFunctionAppWizardContext';
 
 export class FunctionAppHostingPlanStep extends AzureWizardPromptStep<IFunctionAppWizardContext> {
     public async prompt(context: IFunctionAppWizardContext): Promise<void> {
         const placeHolder: string = localize('selectHostingPlan', 'Select a hosting plan.');
-        const enableFlexSetting = await getWorkspaceSetting('enableFlexConsumption');
         const picks: IAzureQuickPickItem<[boolean, RegExp | undefined]>[] = [
             { label: localize('consumption', 'Consumption'), data: [true, undefined] },
+            { label: localize('flexConsumption', 'Flex Consumption'), data: [false, undefined] },
             { label: localize('premium', 'Premium'), data: [false, /^EP$/i] },
             { label: localize('dedicated', 'App Service Plan'), data: [false, /^((?!EP|Y|FC).)*$/i] }
         ];
-
-        if (enableFlexSetting) {
-            picks.splice(1, 0, { label: localize('flexConsumption', 'Flex Consumption (Preview)'), data: [false, undefined] });
-        }
 
         [context.useConsumptionPlan, context.planSkuFamilyFilter] = (await context.ui.showQuickPick(picks, { placeHolder })).data;
         await setLocationsTask(context);
