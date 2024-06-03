c163011f0967ef38fa9872674be27b81c1069d0a
Megan Mott <meganmott@microsoft.com>
2024-06-03 10:10:19 -0700

    Reverse list

--- src/commands/createNewProject/dotnetSteps/DotnetRuntimeStep.ts
+++ src/commands/createNewProject/dotnetSteps/DotnetRuntimeStep.ts
@@ -16,7 +16,7 @@ export class DotnetRuntimeStep extends AzureWizardPromptStep<IProjectWizardConte
     public static async createStep(context: IProjectWizardContext): Promise<DotnetRuntimeStep> {
         if (context.targetFramework) {
             context.targetFramework = typeof context.targetFramework === 'string' ? [context.targetFramework] : context.targetFramework;
-            const runtimes = await getRuntimes(context);
+            const runtimes = (await getRuntimes(context)).reverse(); // reverse so that the latest version is first
             // if a targetFramework was provided from createNewProject
             const workerRuntime = runtimes.find(runtime => context.targetFramework?.includes(runtime.targetFramework));
             if (!workerRuntime) {
