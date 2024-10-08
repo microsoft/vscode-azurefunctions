9090aa988fd78e32caac48454e2aa1b291b28dd1
Nathan Turinski <naturins@microsoft.com>
2024-10-08 09:48:36 -0700

    Update backup templates

--- test/updateBackupTemplates.ts
+++ test/updateBackupTemplates.ts
@@ -34,6 +34,7 @@ suite('Backup templates', () => {
             { language: ProjectLanguage.CSharp, projectTemplateKey: 'net6.0-isolated', versions: [FuncVersion.v4] },
             { language: ProjectLanguage.CSharp, projectTemplateKey: 'net7.0-isolated', versions: [FuncVersion.v4] },
             { language: ProjectLanguage.CSharp, projectTemplateKey: 'net8.0-isolated', versions: [FuncVersion.v4] },
+            { language: ProjectLanguage.CSharp, projectTemplateKey: 'net9.0-isolated', versions: [FuncVersion.v4] },
             { language: ProjectLanguage.Java, versions: [FuncVersion.v4] }
         ];
 
