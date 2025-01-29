468884997549ad0e7f732884fa189eddf5efb623
Nathan Turinski <naturins@microsoft.com>
2025-01-28 18:52:34 -0800

    ffffff

--- extension.bundle.ts
+++ extension.bundle.ts
@@ -54,6 +54,5 @@ export * from './src/vsCodeConfig/extensions';
 export * from './src/vsCodeConfig/launch';
 export * from './src/vsCodeConfig/settings';
 export * from './src/vsCodeConfig/tasks';
-f
 
 // NOTE: The auto-fix action "source.organizeImports" does weird things with this file, but there doesn't seem to be a way to disable it on a per-file basis so we'll just let it happen
