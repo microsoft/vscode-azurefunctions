5f3e223c4ddc0f5c8ffc10facd4ea7e2856be6fc
Phillip Hoff <phillip@orst.edu>
2025-02-07 08:44:11 -0800

    Move emulator image details/defaults to configuration.
    Signed-off-by: Phillip Hoff <phillip@orst.edu>

--- src/tree/durableTaskScheduler/DurableTaskSchedulerEmulatorClient.ts
+++ src/tree/durableTaskScheduler/DurableTaskSchedulerEmulatorClient.ts
@@ -1,4 +1,4 @@
-import { Disposable, type Event, EventEmitter, Uri } from "vscode";
+import { Disposable, type Event, EventEmitter, Uri, workspace } from "vscode";
 import { type ContainerClient } from "./ContainerClient";
 
 export interface DurableTaskSchedulerEmulator {
@@ -15,6 +15,27 @@ export interface DurableTaskSchedulerEmulatorClient {
     getEmulators(): Promise<DurableTaskSchedulerEmulator[]>;
     startEmulator(): Promise<string>;
     stopEmulator(id: string): Promise<void>;
+}
+
+interface ImageFullTag {
+    registry: string;
+    image: string;
+    tag: string;
+}
+
+function getEmulatorFullTag(): ImageFullTag {
+    const configuration = workspace.getConfiguration('azureFunctions');
+
+    // NOTE: Defaults should be specified in package.json.
+    const registry = configuration.get<string>('durableTaskScheduler.emulatorRegistry') as string;
+    const image = configuration.get<string>('durableTaskScheduler.emulatorImage') as string;
+    const tag = configuration.get<string>('durableTaskScheduler.emulatorTag') as string;
+
+    return {
+        registry,
+        image,
+        tag
+    };
 }
 
 export class DockerDurableTaskSchedulerEmulatorClient extends Disposable implements DurableTaskSchedulerEmulatorClient {
@@ -43,9 +64,10 @@ export class DockerDurableTaskSchedulerEmulatorClient extends Disposable impleme
     }
 
     async getEmulators(): Promise<DurableTaskSchedulerEmulator[]> {
+        const { image } = getEmulatorFullTag();
         const containers = await this.dockerClient.getContainers();
 
-        const emulatorContainers = containers.filter(container => container.image.toLowerCase().startsWith('durable-task-scheduler/emulator'));
+        const emulatorContainers = containers.filter(container => container.image.toLowerCase().startsWith(image));
 
         return emulatorContainers.map(container => ({
             dashboardEndpoint: Uri.parse(`http://localhost:${container.ports[8082]}`),
@@ -58,7 +80,8 @@ export class DockerDurableTaskSchedulerEmulatorClient extends Disposable impleme
 
     async startEmulator(): Promise<string> {
         try {
-            const id = await this.dockerClient.runContainer('durabletasks.azurecr.io/durable-task-scheduler/emulator:latest-linux-arm64');
+            const { registry, image, tag } = getEmulatorFullTag();
+            const id = await this.dockerClient.runContainer(`${registry}/${image}:${tag}`);
 
             this.localEmulatorIds.add(id);
 
