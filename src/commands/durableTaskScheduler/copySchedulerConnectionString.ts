8746b9caceea70566e0440e3fcc20c5c112052fc
Phillip Hoff <phillip@orst.edu>
2025-02-10 13:45:02 -0800

    Add DTS management commands (#4370)
    * Scaffold BDP.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Refactor type hierarchy.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Sketch retrieval of task hubs.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Update task hub icon.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Enable "open in portal" command for task hubs.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Scaffold "open in dashboard" command.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Sketch "open in dashboard" implementation.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Move DTS management to separate client type.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Support viewing task hub properties.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Split apart types.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Add file headers.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Consolidate client logic and add localizable strings.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Scaffold creation of task hub.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Sketch creation of schedulers.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Expose DTS creation from common new menu.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Sketch deletion of task hubs.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Sketch deletion of schedulers.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Sketch refreshing models post-creation.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Add tree refresh to remainder of DTS commands.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Provide extended schduler properties.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Add provisioning state when not "normal".
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Move creation command to top of context menu.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Add copy scheduler endpoint command.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Sketch copy connections string command.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Add task hub selection for connection string.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Add async waits.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Wrap deletion in an activity.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Add some robustness to API call failures.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Tweak strings for task hub selection.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Wrap task hub creation with activity.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Wrap task hub deletion with activity.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Add retry for task hub retrieval.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Hide DTS commands from palette.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Add verify providers step to scheduler creation.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Un-hide create scheduler command.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Sketch DTS setting.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Refactor DTS preview setting name.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    * Add preview features enabled check during create.
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>
    
    ---------
    
    Signed-off-by: Phillip Hoff <phillip@orst.edu>

---
+++ src/commands/durableTaskScheduler/copySchedulerConnectionString.ts
@@ -0,0 +1,117 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { type IActionContext } from "@microsoft/vscode-azext-utils";
+import { type DurableTaskSchedulerClient } from "../../tree/durableTaskScheduler/DurableTaskSchedulerClient";
+import { type DurableTaskSchedulerResourceModel } from "../../tree/durableTaskScheduler/DurableTaskSchedulerResourceModel";
+import { localize } from "../../localize";
+import { ext } from "../../extensionVariables";
+import { env, QuickPickItemKind, type QuickPickItem } from "vscode";
+
+export function copySchedulerConnectionStringCommandFactory(schedulerClient: DurableTaskSchedulerClient) {
+    return async (actionContext: IActionContext, scheduler: DurableTaskSchedulerResourceModel | undefined): Promise<void> => {
+        if (!scheduler) {
+            throw new Error(localize('noSchedulerSelectedErrorMessage', 'No scheduler was selected.'));
+        }
+
+        const schedulerJson = await schedulerClient.getScheduler(
+            scheduler.subscription,
+            scheduler.resourceGroup,
+            scheduler.name);
+
+        if (!schedulerJson) {
+            throw new Error(localize('schedulerNotFoundErrorMessage', 'Scheduler does not exist.'));
+        }
+
+        const { endpoint } = schedulerJson.properties;
+
+        const noAuthentication: QuickPickItem = {
+            detail: localize('noAuthenticationDetail', 'No credentials will be used.'),
+            label: localize('noAuthenticationLabel', 'None')
+        }
+
+        const localDevelopment: QuickPickItem = {
+            detail: localize('localDevelopmentDetail', 'Use the credentials of the local developer.'),
+            label: localize('localDevelopmentLabel', 'Local development')
+        };
+
+        const userAssignedManagedIdentity: QuickPickItem = {
+            detail: localize('userAssignedManagedIdentityDetail', 'Use managed identity credentials for a specific client.'),
+            label: localize('userAssignedManagedIdentityLabel', 'User-assigned managed identity')
+        }
+
+        const systemAssignedManagedIdentity: QuickPickItem = {
+            detail: localize('systemAssignedManagedIdentityDetail', 'Use managed identity credentials for a client assigned to a specific Azure resource.'),
+            label: localize('systemAssignedManagedIdentityLabel', 'System-assigned managed identity')
+        }
+
+        const result = await actionContext.ui.showQuickPick(
+            [
+                noAuthentication,
+                localDevelopment,
+                userAssignedManagedIdentity,
+                systemAssignedManagedIdentity
+            ],
+            {
+                canPickMany: false,
+                placeHolder: localize('authenticationTypePlaceholder', 'Select the credentials to be used to connect to the scheduler')
+            });
+
+        let connectionString = `Endpoint=${endpoint};Authentication=`
+
+        if (result === noAuthentication) {
+            connectionString += 'None';
+        }
+        else if (result === localDevelopment) {
+            connectionString += 'DefaultAzure';
+        }
+        else {
+            connectionString += 'ManagedIdentity';
+
+            if (result === userAssignedManagedIdentity) {
+                connectionString += ';ClientID=<ClientID>';
+            }
+        }
+
+        const taskHubs = await schedulerClient.getSchedulerTaskHubs(
+            scheduler.subscription,
+            scheduler.resourceGroup,
+            scheduler.name);
+
+        if (taskHubs.length > 0) {
+
+            const noTaskHubItem: QuickPickItem = {
+                    detail: localize('noTaskHubDetail', 'Do not connect to a specific task hub.'),
+                    label: localize('noTaskHubLabel', 'None')
+                }
+
+            const taskHubItems: QuickPickItem[] =
+                taskHubs.map(taskHub => ({ label: taskHub.name }));
+
+            const taskHubResult = await actionContext.ui.showQuickPick(
+                [
+                    noTaskHubItem,
+                    {
+                        kind: QuickPickItemKind.Separator,
+                        label: localize('taskHubSepratorLabel', 'Task Hubs')
+                    },
+                    ...taskHubItems
+                ],
+                {
+                    canPickMany: false,
+                    placeHolder: localize('taskHubSelectionPlaceholder', 'Select a task hub to connect to')
+                });
+
+            if (taskHubResult && taskHubResult !== noTaskHubItem) {
+                connectionString += `;TaskHub=${taskHubResult.label}`;
+            }
+        }
+
+        await env.clipboard.writeText(connectionString);
+
+        ext.outputChannel.show();
+        ext.outputChannel.appendLog(localize('schedulerConnectionStringCopiedMessage', 'Connection string copied to clipboard: {0}', connectionString));
+    }
+}
