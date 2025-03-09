8446d46448d2b628f181f0235b1f160704dd058f
Phillip Hoff <phillip@orst.edu>
2025-03-08 16:04:29 -0800

    Add missing headers.
    Signed-off-by: Phillip Hoff <phillip@orst.edu>

--- src/tree/durableTaskScheduler/DurableTaskSchedulerWorkspaceResourceProvider.ts
+++ src/tree/durableTaskScheduler/DurableTaskSchedulerWorkspaceResourceProvider.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import { type WorkspaceResource, type WorkspaceResourceProvider } from "@microsoft/vscode-azureresources-api";
 import { type Event, type ProviderResult } from "vscode";
 import { DurableTaskSchedulerEmulatorsWorkspaceResource } from "./DurableTaskSchedulerEmulatorsWorkspaceResource";
