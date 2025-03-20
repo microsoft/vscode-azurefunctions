952d07e22017862b7f8d4be3b32dad0146b92d01
Nathan <naturins@microsoft.com>
2025-03-20 16:34:46 -0700

    Add Identity node to Function Apps (#4382)
    * Skip test to let artifacts publish
    
    * Fix stupid build issue
    
    * UI for managed identity
    
    * Add system identity enablement, still need to do managed wizard
    
    * WIP MI with azureutils package impl
    
    * Use shared package logic to create role definitions
    
    * Minor text edits
    
    * Remove some copy/pasted stuff
    
    * Remove setting
    
    * Remove unused string
    
    * Update to use real npm package
    
    * Feedback
    
    * PR feedback
    
    * Update azure utils package
    
    * Remove child under system assigned (disabled) node
    
    * PR feedback
    
    * Fix package-lock.json
    
    * Use prerelease version
    
    * cliFeed response
    
    * Stringify json
    
    * JSON parse bodyAsString
    
    * Remove serviceconnector
    
    * PR feedback

---
+++ src/tree/remoteProject/ManagedIdentityTreeItem.ts
@@ -0,0 +1,55 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { type ParsedSite } from '@microsoft/vscode-azext-azureappservice';
+import { AzExtParentTreeItem, createContextValue, type AzExtTreeItem, type IActionContext, type TreeItemIconPath } from '@microsoft/vscode-azext-utils';
+import { ThemeIcon } from 'vscode';
+import { localize } from '../../localize';
+import { type SlotTreeItem } from '../SlotTreeItem';
+import { createSystemIdentityTreeItem } from './SystemIdentityTreeItemBase';
+import { UserAssignedIdentitiesTreeItem } from './UserAssignedIdentitiesTreeItem';
+
+export class ManagedIdentityTreeItem extends AzExtParentTreeItem {
+    public readonly label: string = localize('Identity', 'Identity');
+    public static contextValue: string = 'azFuncManagedIdentity';
+    public readonly parent: SlotTreeItem;
+    public readonly site: ParsedSite;
+    public suppressMaskLabel: boolean = true;
+
+    constructor(parent: SlotTreeItem) {
+        super(parent);
+    }
+
+    public get contextValue(): string {
+        return createContextValue([ManagedIdentityTreeItem.contextValue]);
+    }
+
+    public get id(): string {
+        return 'appIdentityView';
+    }
+
+    public get iconPath(): TreeItemIconPath {
+        return new ThemeIcon('key');
+    }
+
+    public async refreshImpl(context: IActionContext): Promise<void> {
+        await this.loadAllChildren(context);
+    }
+
+    public hasMoreChildrenImpl(): boolean {
+        return false;
+    }
+
+    public async loadMoreChildrenImpl(_clearCache: boolean): Promise<AzExtTreeItem[]> {
+        const systemIdentitiesTreeItem: AzExtTreeItem = createSystemIdentityTreeItem(this);
+        const userAssignedIdentitiesTreeItem = new UserAssignedIdentitiesTreeItem(this.parent);
+        return [systemIdentitiesTreeItem, userAssignedIdentitiesTreeItem];
+    }
+
+    public compareChildrenImpl(_ti1: AzExtTreeItem, _ti2: AzExtTreeItem): number {
+        // don't sort
+        return 0;
+    }
+}
---
+++ src/tree/remoteProject/UserAssignedIdentityTreeItem.ts
@@ -0,0 +1,48 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { type Identity } from '@azure/arm-msi';
+import { createPortalUri, createRoleDefinitionsItems, RoleDefinitionsTreeItem, type RoleDefinitionsItem } from '@microsoft/vscode-azext-azureutils';
+import { AzExtParentTreeItem, nonNullProp, type AzExtTreeItem, type IActionContext, type TreeItemIconPath } from '@microsoft/vscode-azext-utils';
+import { type AzureSubscription } from '@microsoft/vscode-azureresources-api';
+import { type Uri } from 'vscode';
+import { localize } from '../../localize';
+import { treeUtils } from '../../utils/treeUtils';
+import { type UserAssignedIdentitiesTreeItem } from './UserAssignedIdentitiesTreeItem';
+
+export class UserAssignedIdentityTreeItem extends AzExtParentTreeItem {
+    readonly portalUrl: Uri;
+    public readonly parent: UserAssignedIdentitiesTreeItem;
+    public readonly identity: Identity;
+    public contextValue: string = 'userAssignedIdentity';
+    children: AzExtTreeItem[] = [];
+    public constructor(parent: UserAssignedIdentitiesTreeItem, identity: Identity) {
+        super(parent);
+        this.parent = parent;
+        this.identity = identity;
+        this.portalUrl = createPortalUri(parent.parent.subscription as unknown as AzureSubscription, nonNullProp(identity, 'id'));
+    }
+
+    public get iconPath(): TreeItemIconPath {
+        return treeUtils.getIconPath("ManagedIdentityUserAssignedIdentities");
+    }
+
+    public async loadMoreChildrenImpl(_clearCache: boolean, context: IActionContext): Promise<AzExtTreeItem[]> {
+        const roleDefinitionsItem: RoleDefinitionsItem[] = await createRoleDefinitionsItems(context, this.parent.subscription, this.identity);
+        return roleDefinitionsItem.map(rd => new RoleDefinitionsTreeItem(this, rd));
+    }
+
+    public hasMoreChildrenImpl(): boolean {
+        return false
+    }
+
+    public get id(): string {
+        return this.identity.principalId || '';
+    }
+
+    public get label(): string {
+        return this.identity.name || this.identity.principalId || this.identity.tenantId || localize('userAssigned', 'User assigned');
+    }
+}
