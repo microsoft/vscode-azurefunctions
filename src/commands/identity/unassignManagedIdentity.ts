9c8b60c5a62c0a5ca1f481a8c36fc817e413a078
Nathan Turinski <naturins@microsoft.com>
2025-04-07 13:35:00 -0700

    Add modal prompt before unassigning MI

--- src/commands/identity/unassignManagedIdentity.ts
+++ src/commands/identity/unassignManagedIdentity.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { AzureWizard, type IActionContext } from '@microsoft/vscode-azext-utils';
+import { AzureWizard, DialogResponses, type IActionContext } from '@microsoft/vscode-azext-utils';
 import { localize } from '../../localize';
 import { type UserAssignedIdentityTreeItem } from '../../tree/remoteProject/UserAssignedIdentityTreeItem';
 import { type SlotTreeItem } from '../../tree/SlotTreeItem';
@@ -21,6 +21,14 @@ export async function unassignManagedIdentity(context: IActionContext, node: Use
         ...node.subscription,
         ...(await createActivityContext())
     }
+
+    await context.ui.showWarningMessage(
+        localize('unassignManagedIdentityWarning', "'{0}' will not be able to request access tokens for the user assigned managed identity '{1}'. Do you want to continue?",
+            slotTreeItem.site?.fullName,
+            node.identity.name),
+        { modal: true },
+        DialogResponses.yes
+    );
 
     const title: string = localize('assignManagedIdentity', 'Unassign User Assigned Identity to Function App');
     const wizard: AzureWizard<ManagedIdentityAssignContext> = new AzureWizard(wizardContext, {
