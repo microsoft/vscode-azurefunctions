2cc041d06f3aacaec40e764045cfd5aab08887a8
Phillip Hoff <phillip@orst.edu>
2025-05-16 08:58:38 -0700

    More updates per PR feedback.

--- src/tree/durableTaskScheduler/DurableTaskSchedulerErrorWorkspaceResourceModel.ts
+++ src/tree/durableTaskScheduler/DurableTaskSchedulerErrorWorkspaceResourceModel.ts
@@ -3,16 +3,12 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { type ProviderResult, ThemeIcon, TreeItem } from "vscode";
+import { ThemeIcon, TreeItem } from "vscode";
 import { type DurableTaskSchedulerWorkspaceResourceModel } from "./DurableTaskSchedulerWorkspaceResourceModel";
 
 export class DurableTaskSchedulerErrorWorkspaceResourceModel implements DurableTaskSchedulerWorkspaceResourceModel {
     constructor(
         private readonly error: string) {
-    }
-
-    getChildren(): ProviderResult<DurableTaskSchedulerWorkspaceResourceModel[]> {
-        return [];
     }
 
     getTreeItem(): TreeItem | Thenable<TreeItem> {
