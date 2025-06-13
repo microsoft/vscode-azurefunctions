a0f194bc87a6e9753f9fe198ee87e65368f6a9fd
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2025-06-13 16:08:27 -0700

    Don't need to update this file

--- src/commands/deploy/shouldValidateConnection.ts
+++ src/commands/deploy/shouldValidateConnection.ts
@@ -3,22 +3,18 @@ import { type SiteClient } from "@microsoft/vscode-azext-azureappservice";
 import { ConnectionKey, DurableBackend, type DurableBackendValues } from "../../constants";
 import { getEventHubName } from "../appSettings/connectionSettings/eventHubs/validateEventHubsConnection";
 
-type ShouldValidateConnections = {
+export interface IShouldValidateConnection {
     shouldValidateEventHubs: boolean;
     shouldValidateSqlDb: boolean;
 }
 
-export async function shouldValidateConnections(durableStorageType: DurableBackendValues | undefined, client: SiteClient, projectPath: string): Promise<ShouldValidateConnections> {
+export async function shouldValidateConnections(durableStorageType: DurableBackendValues | undefined, client: SiteClient, projectPath: string): Promise<IShouldValidateConnection> {
     const app: StringDictionary = await client.listApplicationSettings();
-
-    // Event Hubs
     const remoteEventHubsConnection: string | undefined = app?.properties?.[ConnectionKey.EventHubs];
-    const eventHubName: string | undefined = await getEventHubName(projectPath);
-    const shouldValidateEventHubs: boolean = durableStorageType === DurableBackend.Netherite && (!eventHubName || !remoteEventHubsConnection);
-
-    // SQL
     const remoteSqlDbConnection: string | undefined = app?.properties?.[ConnectionKey.SQL];
-    const shouldValidateSqlDb: boolean = durableStorageType === DurableBackend.SQL && !remoteSqlDbConnection;
+    const eventHubName: string | undefined = await getEventHubName(projectPath);
 
+    const shouldValidateEventHubs: boolean = durableStorageType === DurableBackend.Netherite && (!eventHubName || !remoteEventHubsConnection);
+    const shouldValidateSqlDb: boolean = durableStorageType === DurableBackend.SQL && !remoteSqlDbConnection;
     return { shouldValidateEventHubs, shouldValidateSqlDb };
 }
