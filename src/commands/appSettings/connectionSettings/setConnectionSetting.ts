7b9857026190f76d409164e198daffd554647850
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2025-06-29 18:21:09 -0500

    Add comment to utility function

--- src/commands/appSettings/connectionSettings/setConnectionSetting.ts
+++ src/commands/appSettings/connectionSettings/setConnectionSetting.ts
@@ -7,7 +7,11 @@ import { CodeAction } from "../../../constants";
 import { MismatchBehavior, setLocalAppSetting } from "../../../funcConfig/local.settings";
 import { type ISetConnectionSettingContext } from "./ISetConnectionSettingContext";
 
-// Todo: Add better comment about what this does
+/**
+ * Automatically sets the correct connection setting based on whether the code action is running during `Debug` or `Deploy`.
+ * - If the action is `Deploy`, it sets the key/value directly on the context to be later added as an app setting.
+ * - If the action is `Debug`, it updates the `local.settings.json` file for local development, overwriting any existing values.
+ */
 export async function setConnectionSetting(context: ISetConnectionSettingContext, key: string, value: string): Promise<void> {
     if (context.action === CodeAction.Deploy) {
         context[key] = value;
