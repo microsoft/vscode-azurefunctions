df78db482c57d299b87b97a904caf19b91f4b099
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2025-08-04 12:56:07 -0700

    Improve support for assigning UAID. Supporting adding newly created UAID.

--- src/commands/identity/FunctionAppUserAssignedIdentitiesListStep.ts
+++ src/commands/identity/FunctionAppUserAssignedIdentitiesListStep.ts
@@ -5,11 +5,12 @@
 
 import { type ManagedServiceIdentityClient } from '@azure/arm-msi';
 import { type ParsedSite } from '@microsoft/vscode-azext-azureappservice';
-import { createAuthorizationManagementClient, createManagedServiceIdentityClient, parseAzureResourceId, uiUtils, type ParsedAzureResourceId, type Role } from '@microsoft/vscode-azext-azureutils';
-import { ActivityChildItem, ActivityChildType, activityInfoContext, activityInfoIcon, AzureWizardPromptStep, createContextValue, nonNullProp, prependOrInsertAfterLastInfoChild, type ActivityInfoChild, type IAzureQuickPickItem } from '@microsoft/vscode-azext-utils';
-import { ext } from '../../../extensionVariables';
-import { localize } from '../../../localize';
-import { type IFunctionAppUserAssignedIdentitiesContext } from './IFunctionAppUserAssignedIdentitiesContext';
+import { createAuthorizationManagementClient, createManagedServiceIdentityClient, parseAzureResourceId, uiUtils, UserAssignedIdentityListStep, type ParsedAzureResourceId, type Role } from '@microsoft/vscode-azext-azureutils';
+import { ActivityChildItem, ActivityChildType, activityInfoContext, activityInfoIcon, AzureWizardPromptStep, createContextValue, nonNullProp, prependOrInsertAfterLastInfoChild, type ActivityInfoChild, type IAzureQuickPickItem, type IWizardOptions } from '@microsoft/vscode-azext-utils';
+import { ext } from '../../extensionVariables';
+import { localize } from '../../localize';
+import { type ManagedIdentityAssignContext } from './ManagedIdentityAssignContext';
+import { ManagedIdentityAssignStep } from './ManagedIdentityAssignStep';
 
 /**
  * Wizard step to select a user-assigned managed identity from the parsed site of a function app.
@@ -20,30 +21,22 @@ import { type IFunctionAppUserAssignedIdentitiesContext } from './IFunctionAppUs
  *
  * @populates `context.managedIdentity`
  */
-export class FunctionAppUserAssignedIdentitiesListStep<T extends IFunctionAppUserAssignedIdentitiesContext> extends AzureWizardPromptStep<T> {
+export class FunctionAppUserAssignedIdentitiesListStep<T extends ManagedIdentityAssignContext> extends AzureWizardPromptStep<T> {
     private _msiClient: ManagedServiceIdentityClient;
-    private _hasTargetRole?: boolean;
 
-    constructor(readonly targetRole?: Role) {
+    constructor(
+        readonly targetRole?: Role,
+        readonly options?: { identityAssignStepPriority?: number },
+    ) {
         super();
-    }
-
-    /**
-     * Indicates whether there is at least one user-assigned identity on the function app with the provided role.
-     * If no role is provided, or if the step has not yet been run, this will return `undefined`.
-     */
-    get hasIdentityWithTargetRole(): boolean | undefined {
-        return this._hasTargetRole;
     }
 
     // Verify if any of the existing user assigned identities for the function app have the required role already
     public async configureBeforePrompt(context: T): Promise<void> {
-        if (!this.targetRole || !this.targetRole?.scopeId) {
-            this._hasTargetRole = undefined;
+        if (!this.targetRole?.scopeId) {
             return;
         }
 
-        this._hasTargetRole = false;
         this._msiClient ??= await createManagedServiceIdentityClient(context);
         const amClient = await createAuthorizationManagementClient(context);
 
@@ -52,6 +45,7 @@ export class FunctionAppUserAssignedIdentitiesListStep<T extends IFunctionAppUse
         const identityIds: string[] = Object.keys(site.identity?.userAssignedIdentities ?? {}) ?? [];
         context.telemetry.properties.functionAppUserAssignedIdentityCount = String(identityIds.length);
 
+        let hasTargetRole: boolean = false;
         for (const identityId of identityIds) {
             const uaid = site.identity?.userAssignedIdentities?.[identityId];
             const roleAssignments = await uiUtils.listAllIterator(amClient.roleAssignments.listForScope(
@@ -65,20 +59,21 @@ export class FunctionAppUserAssignedIdentitiesListStep<T extends IFunctionAppUse
             if (roleAssignments.some(r => !!r.roleDefinitionId?.endsWith(role.roleDefinitionId))) {
                 const parsedIdentity = parseAzureResourceId(identityId);
                 context.managedIdentity = await this._msiClient.userAssignedIdentities.get(parsedIdentity.resourceGroup, parsedIdentity.resourceName);
-                this._hasTargetRole = true;
+                hasTargetRole = true;
                 break;
             }
         }
 
-        context.telemetry.properties.functionAppHasIdentityWithTargetRole = String(this.hasIdentityWithTargetRole);
+        context.telemetry.properties.functionAppHasIdentityWithTargetRole = String(hasTargetRole);
 
-        if (this.hasIdentityWithTargetRole) {
+        if (hasTargetRole) {
             prependOrInsertAfterLastInfoChild(context,
                 new ActivityChildItem({
+                    stepId: this.id,
                     label: localize('useIdentityWithRole', 'Use identity "{0}" with role "{1}"', context.managedIdentity?.name, this.targetRole.roleDefinitionName),
                     contextValue: createContextValue(['functionAppUserAssignedIdentitiesListStepItem', activityInfoContext]),
                     activityType: ActivityChildType.Info,
-                    iconPath: activityInfoIcon
+                    iconPath: activityInfoIcon,
                 }) as ActivityInfoChild,
             );
             ext.outputChannel.appendLog(localize('foundIdentity', 'Located existing user assigned identity "{0}" with role "{1}".', context.managedIdentity?.name, this.targetRole.roleDefinitionName));
@@ -89,11 +84,13 @@ export class FunctionAppUserAssignedIdentitiesListStep<T extends IFunctionAppUse
 
     public async prompt(context: T): Promise<void> {
         const site: ParsedSite = nonNullProp(context, 'site');
-        const identityId: string = (await context.ui.showQuickPick(await this.getPicks(site), {
+        const identityId: string | undefined = (await context.ui.showQuickPick(await this.getPicks(site), {
             placeHolder: localize('selectFunctionAppIdentity', 'Select a function app identity for new role assignments'),
-            // Todo: Remove when create + assign is implemented
-            noPicksMessage: localize('noUserAssignedIdentities', 'No identities found. Add a user assigned identity to the function app before proceeding.'),
         })).data;
+
+        if (!identityId) {
+            return;
+        }
 
         const parsedIdentity: ParsedAzureResourceId = parseAzureResourceId(identityId);
         this._msiClient ??= await createManagedServiceIdentityClient(context);
@@ -106,14 +103,34 @@ export class FunctionAppUserAssignedIdentitiesListStep<T extends IFunctionAppUse
         return !context.managedIdentity;
     }
 
-    private async getPicks(site: ParsedSite): Promise<IAzureQuickPickItem<string>[]> {
-        return Object.keys(site.identity?.userAssignedIdentities ?? {}).map((id) => {
-            const parsedResource: ParsedAzureResourceId = parseAzureResourceId(id);
-            return {
-                label: parsedResource.resourceName,
-                description: parsedResource.resourceGroup,
-                data: id,
-            };
-        });
+    public async getSubWizard(context: T): Promise<IWizardOptions<T> | undefined> {
+        if (context.managedIdentity) {
+            return undefined;
+        }
+
+        return {
+            promptSteps: [new UserAssignedIdentityListStep()],
+            executeSteps: [new ManagedIdentityAssignStep({ priority: this.options?.identityAssignStepPriority })],
+        };
+    }
+
+    private async getPicks(site: ParsedSite): Promise<IAzureQuickPickItem<string | undefined>[]> {
+        const picks: IAzureQuickPickItem<string | undefined>[] = [{
+            label: localize('assignIdentity', '$(plus) Assign new user-assigned identity'),
+            data: undefined,
+        }];
+
+        return picks.concat(
+            Object
+                .keys(site.identity?.userAssignedIdentities ?? {})
+                .map((id) => {
+                    const parsedResource: ParsedAzureResourceId = parseAzureResourceId(id);
+                    return {
+                        label: parsedResource.resourceName,
+                        description: parsedResource.resourceGroup,
+                        data: id,
+                    };
+                }),
+        );
     }
 }
