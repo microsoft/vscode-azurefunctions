c72a8e0aff8071777bd091e2fd4692abfbc990a7
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2025-08-06 10:58:05 -0700

    Wordings v3

--- src/commands/appSettings/connectionSettings/durableTaskScheduler/DTSConnectionListStep.ts
+++ src/commands/appSettings/connectionSettings/durableTaskScheduler/DTSConnectionListStep.ts
@@ -44,7 +44,7 @@ export class DTSConnectionListStep<T extends IDTSConnectionWizardContext> extend
 
         buttons.push(skipForNow);
 
-        const message: string = localize('selectDTSConnection', 'Durable Functions requires a Durable Task Scheduler connection.');
+        const message: string = localize('selectDTSConnection', 'Durable Functions requires a Durable Task Scheduler to be configured.');
         context.dtsConnectionType = (await context.ui.showWarningMessage(message, { modal: true }, ...buttons) as {
             title: string;
             data: ConnectionType;
