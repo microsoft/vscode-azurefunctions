f05aabc6c3f917398f4c0600f6650570acb749d6
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2025-09-05 20:26:47 +0900

    More refactoring

--- src/commands/createFunctionApp/containerImage/detectContainerizedProject.ts
+++ src/commands/createFunctionApp/containerImage/detectContainerizedProject.ts
@@ -2,7 +2,7 @@
 *  Copyright (c) Microsoft Corporation. All rights reserved.
 *  Licensed under the MIT License. See License.md in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
-import { AzExtFsExtra } from '@microsoft/vscode-azext-utils';
+import { AzExtFsExtra, type IActionContext } from '@microsoft/vscode-azext-utils';
 import * as vscode from 'vscode';
 import { dockerfileGlobPattern } from '../../../constants';
 import { getAzureContainerAppsApi } from '../../../getExtensionApi';
@@ -11,7 +11,7 @@ import { type ICreateFunctionAppContext } from '../../../tree/SubscriptionTreeIt
 import { findFiles, getRootWorkspaceFolder } from '../../../utils/workspace';
 import { tryGetFunctionProjectRoot } from '../../createNewProject/verifyIsProject';
 
-export async function detectDockerfile(context: ICreateFunctionAppContext): Promise<void> {
+export async function detectAndConfigureContainerizedProject(context: ICreateFunctionAppContext): Promise<void> {
     if (!vscode.workspace.workspaceFolders?.length) {
         return;
     }
@@ -24,24 +24,25 @@ export async function detectDockerfile(context: ICreateFunctionAppContext): Prom
         return;
     }
 
+    const useContainerImage: boolean = await askUseContainerImage(context);
+    if (!useContainerImage) {
+        return;
+    }
+
     // Todo: If there's multiple dockerfiles, we should probably prompt
     context.dockerfilePath = dockerfiles[0].fsPath;
 
-    // prompt user to proceed with containerized function app creation
-    if (context.dockerfilePath) {
-        const placeHolder: string = localize('detectedDockerfile', 'Dockerfile detected. What would you like to deploy?');
-        const containerImageButton: vscode.MessageItem = { title: localize('containerImage', 'Container Image') };
-        const codeButton: vscode.MessageItem = { title: localize('code', 'Code') };
-        const buttons: vscode.MessageItem[] = [containerImageButton, codeButton];
-        const result: vscode.MessageItem = await context.ui.showWarningMessage(placeHolder, { modal: true }, ...buttons);
+    // ensure container apps extension is installed before proceeding
+    await getAzureContainerAppsApi(context);
+}
 
-        // if yes, ensure container apps extension is installed before proceeding
-        if (result === containerImageButton) {
-            await getAzureContainerAppsApi(context);
-        } else if (result === codeButton) {
-            context.dockerfilePath = undefined;
-        }
-    }
+async function askUseContainerImage(context: IActionContext): Promise<boolean> {
+    const placeHolder: string = localize('detectedDockerfile', 'Dockerfile detected. What would you like to deploy?');
+    const containerImageButton: vscode.MessageItem = { title: localize('containerImage', 'Container Image') };
+    const codeButton: vscode.MessageItem = { title: localize('code', 'Code') };
+    const buttons: vscode.MessageItem[] = [containerImageButton, codeButton];
+    const result: vscode.MessageItem = await context.ui.showWarningMessage(placeHolder, { modal: true }, ...buttons);
+    return result === containerImageButton;
 }
 
 // Todo: Seems like this isn't being used, but could use to filter which dockerfiles to show
