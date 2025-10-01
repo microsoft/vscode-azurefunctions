9df2dbf9dd45e8e37622db444571b1f93842a33f
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2025-10-01 15:24:35 -0700

    WIP

--- src/debug/validatePreDebug/storageProviders/emulators/LocalEmulatorsListStep.ts
+++ src/debug/validatePreDebug/storageProviders/emulators/LocalEmulatorsListStep.ts
@@ -8,17 +8,20 @@ import { StorageProviderType } from '../../../../constants';
 import { type IPreDebugValidateContext } from '../../IPreDebugValidateContext';
 
 export class LocalEmulatorsListStep<T extends IPreDebugValidateContext> extends AzureWizardPromptStep<T> {
-    private _emulators: StorageProviderType[] = [];
+    private _emulatorsToStart: StorageProviderType[] = [];
 
     public async prompt(context: T): Promise<void> {
-        this._emulators = [];
+        // Maybe this first part should be configureBeforePrompt
+        this._emulatorsToStart = [];
 
         switch (context.durableStorageType) {
             case StorageProviderType.DTS:
-                this._emulators.push(StorageProviderType.DTS);
+                this._emulatorsToStart.push(StorageProviderType.DTS);
+                // Todo: Verify if emulator needs starting
                 break;
             case StorageProviderType.Netherite:
-                this._emulators.push(StorageProviderType.Netherite);
+                this._emulatorsToStart.push(StorageProviderType.Netherite);
+                // Todo: Verify if emulator needs starting
                 break;
             case StorageProviderType.SQL:
                 // Todo: Add SQL emulator support
@@ -27,7 +30,8 @@ export class LocalEmulatorsListStep<T extends IPreDebugValidateContext> extends 
             default:
         }
 
-        this._emulators.push(StorageProviderType.Storage);
+        this._emulatorsToStart.push(StorageProviderType.Storage);
+        // Todo: Verify if emulator needs starting
     }
 
     public shouldPrompt(): boolean {
