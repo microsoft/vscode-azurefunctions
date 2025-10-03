836e5f710a62417c75540236626aeeb98ecd927c
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2025-10-03 12:22:01 -0700

    WIP

--- gulpfile.ts
+++ gulpfile.ts
@@ -10,9 +10,9 @@ import * as gulp from 'gulp';
 import * as chmod from 'gulp-chmod';
 import * as decompress from 'gulp-decompress';
 import * as filter from 'gulp-filter';
+import * as https from 'https';
 import * as os from 'os';
 import * as path from 'path';
-import * as request from 'request';
 import * as buffer from 'vinyl-buffer';
 import * as source from 'vinyl-source-stream';
 
@@ -25,7 +25,7 @@ async function prepareForWebpack(): Promise<void> {
     await fse.writeFile(mainJsPath, contents);
 }
 
-let downloadLink;
+let downloadLink: string;
 async function getFuncLink() {
     const client = new msRest.ServiceClient();
     const cliFeed = JSON.parse((await client.sendRequest({ method: 'GET', url: 'https://aka.ms/V00v5v' })).bodyAsText as string);
@@ -53,29 +53,56 @@ function matchesCliFeedOS(platform: string) {
     }
 }
 
-function installFuncCli() {
+async function cleanReadme() {
+    const readmePath: string = path.join(__dirname, 'README.md');
+    let data: string = (await fse.readFile(readmePath)).toString();
+    data = data.replace(/<!-- region exclude-from-marketplace -->.*?<!-- endregion exclude-from-marketplace -->/gis, '');
+    await fse.writeFile(readmePath, data);
+}
+
+async function installFuncCli() {
     const funcDir = path.join(os.homedir(), 'tools', 'func');
     if (fse.pathExistsSync(funcDir)) {
         console.log('Removing old install of func.');
         fse.removeSync(funcDir);
     }
 
-    const funcFilter = filter('func', { restore: true });
-    return request(downloadLink)
-        .pipe(source('funccli.zip'))
-        .pipe(buffer())
-        .pipe(decompress())
-        .pipe(funcFilter)
-        .pipe(chmod({ execute: true }))
-        .pipe(funcFilter.restore)
-        .pipe(gulp.dest(funcDir));
+    try {
+        await httpsGetAndInstallFuncCli(downloadLink, funcDir);
+        console.log('Successfully installed the func CLI at ' + funcDir);
+    } catch (e) {
+        console.log('Failed to install the func CLI at ' + funcDir);
+        console.error(e);
+    }
 }
 
-async function cleanReadme() {
-    const readmePath: string = path.join(__dirname, 'README.md');
-    let data: string = (await fse.readFile(readmePath)).toString();
-    data = data.replace(/<!-- region exclude-from-marketplace -->.*?<!-- endregion exclude-from-marketplace -->/gis, '');
-    await fse.writeFile(readmePath, data);
+function httpsGetAndInstallFuncCli(url: string, funcDir: string): Promise<void> {
+    const funcFilter = filter('func', { restore: true });
+
+    return new Promise((resolve, reject) => {
+        const request = https.get(url, (response) => {
+            if (response.statusCode && (response.statusCode < 200 || response.statusCode >= 300)) {
+                reject(new Error('Request for func CLI responded with status code: ' + response.statusCode));
+                return;
+            }
+
+            const pipeline: NodeJS.ReadWriteStream = response
+                .pipe(source('funccli.zip'))
+                .pipe(buffer())
+                .pipe(decompress())
+                .pipe(funcFilter)
+                .pipe(chmod({ execute: true }))
+                .pipe(funcFilter.restore)
+                .pipe(gulp.dest(funcDir));
+
+            response.on('error', (error) => reject(new Error('Response error: ' + error.message)));
+            pipeline.on('error', (error) => reject(new Error('Pipeline processing error: ' + error.message)));
+            pipeline.on('end', resolve);
+            pipeline.on('finish', resolve);
+        });
+
+        request.on('error', (error) => reject(new Error('Failed to make GET request: ' + error.message)));
+    });
 }
 
 exports['webpack-dev'] = gulp.series(prepareForWebpack, () => gulp_webpack('development'));
