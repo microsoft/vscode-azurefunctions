002aa8210c7dae6c19d5d7cc47ecd29a203ef3c8
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2026-01-21 14:13:19 -0800

    Reorganize constants

--- test/constants.ts
+++ test/constants.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-// Start section create-new-project
+// #region create-new-project
 // Project type picks
 export const durableOrchestratorPick: RegExp = /Durable Functions Orch/i; // Sometimes this is "orchestrator" or "orchestration" depending on the template feed
 export const durableAzureStoragePick: RegExp = /Azure Storage/i;
@@ -17,8 +17,15 @@ export const jsLanguagePick: RegExp = /JavaScript/i;
 // Framework picks
 export const jsModelV4Pick: RegExp = /v4/i;
 
-// Start section create-function-app
-export const locationDefaultPick: RegExp = /West US 2/i;
+// #endregion
 
+// ----------------------------
+
+// #region create-function-app
 // Default runtime picks
 export const nodeRuntimePick: RegExp = /Node\.js(\s)?22/i;
+
+// Location picks
+export const locationDefaultPick: RegExp = /West US 2/i;
+
+// #endregion
