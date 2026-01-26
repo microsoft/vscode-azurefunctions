38ba47cf6fe1bdb8e139518311d2c016c054b27b
Nathan <naturins@microsoft.com>
2026-01-26 11:55:38 -0800

    Remove duplicate copyright notice
    Removed duplicate copyright notice from projectContextValues.ts

--- src/tree/projectContextValues.ts
+++ src/tree/projectContextValues.ts
@@ -3,11 +3,6 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
 const contextValueSeparator: string = ';';
 const contextValuePrefix: string = 'azFunc';
 
