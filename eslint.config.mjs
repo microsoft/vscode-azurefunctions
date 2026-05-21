16fb4898424e939af2c7df9f468383f6efd18ff2
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2026-05-21 12:43:36 -0400

    Ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,7 @@ import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
     {
-        ignores: ['test/**', 'webpack.config.js', '.eslintrc.js', 'scripts/**', 'test-feed-mirrors.mjs'],
+        ignores: ['test/**', 'webpack.config.js', '.eslintrc.js', 'scripts/**', '.config/**'],
     },
     ...azExtEslintRecommended,
     {
